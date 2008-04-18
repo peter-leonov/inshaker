fc5b4e03578a88f21b0fbe42bc203001df7b29ac
Vasily Mikhailitchenko <vaskas@programica.ru>
2008-04-18 11:17:30 +0000

    printing of cocktails

--- htdocs/js/printer/printer.js
+++ htdocs/js/printer/printer.js
@@ -3,14 +3,56 @@ var Printer = {
     ID_COCKTAILS_LIST : 'cocktails_list',
     ID_INGREDS_LIST   : 'ingredients_list',
     ID_TOOLS_LIST     : 'tools_list',
+    ID_RECEIPT        : 'receipt',
+    
+    ID_COCKTAIL_NAME  : 'cocktail_name',
+    ID_COCKTAIL_IMG   : 'cocktail_img',
+    ID_INGREDS_IMGS   : 'cocktail_ingreds',
+
+    IMG_COCKTAIL_PRFX : '/i/cocktail/b/',
+    IMG_INGRED_PRFX   : '/i/merchandise/ingredients/',
     IMG_MARKER        : '/t/print/li.gif',
 
     cartData : {},
 
-    init: function(context) {
-        if(this[context+"Init"]) this[context+"Init"]();
+    init: function(context, param) {
+        if(this[context+"Init"]) this[context+"Init"](param);
     },
 
+    cocktailInit: function(param){
+        var cocktail = null;
+        for(name in cocktails) {
+            if(cocktails[name].name_eng == param) {
+                cocktail = cocktails[name];
+                break;
+            }
+        }
+        var receiptRoot   = $(this.ID_RECEIPT);
+        var ingredsRoot   = $(this.ID_INGREDS_LIST);
+        var toolsRoot     = $(this.ID_TOOLS_LIST);
+        var imgsRoot      = $(this.ID_INGREDS_IMGS);
+        
+       $(this.ID_COCKTAIL_NAME).innerHTML = cocktail.name;
+       $(this.ID_COCKTAIL_IMG).src = this.IMG_COCKTAIL_PRFX + cocktail.name_eng.htmlName() + ".png";
+       for(var i = 0; i < cocktail.receipt.length; i++){
+            receiptRoot.appendChild(this.createReceiptElement(cocktail.receipt[i]));
+       }
+
+       for(var i = 0; i < cocktail.ingredients.length; i++){
+           var last = (i == (cocktail.ingredients.length - 1));
+           ingredsRoot.appendChild(this.createIngredPairElement(cocktail.ingredients[i], last));
+       }
+
+       for(var i = 0; i < cocktail.tools.length; i++){
+           var last = (i == (cocktail.tools.length - 1));
+           toolsRoot.appendChild(this.createToolElement(cocktail.tools[i], last));
+       }
+
+       for(var i = 0; i < cocktail.ingredients.length; i++) {
+            imgsRoot.appendChild(this.createIngredImage(cocktail.ingredients[i][0]));
+       }
+    },
+    
     cartInit: function(){
         if(Cookie.get(GoodHelper.CART_COOKIE)){
             this.cartData = Cookie.get(GoodHelper.CART_COOKIE);
@@ -82,6 +124,37 @@ var Printer = {
        return dd;
     },
 
+    createReceiptElement: function(line) {
+        var li = document.createElement("li");
+        li.innerHTML = line;
+        return li; 
+    },
+    
+    createIngredImage: function(name){
+       var img = new Image();
+       img.src = this.IMG_INGRED_PRFX + name.trans() + "_small.png";
+       return img;
+    },
+
+    createIngredPairElement: function(pair, last){
+        var dd = document.createElement("dd");
+        if(last) dd.className = "last";
+        var div = document.createElement("div");
+        div.className = "txt";
+        var img = new Image();
+        img.src = this.IMG_MARKER;
+        div.appendChild(img);
+        div.appendChild(document.createTextNode(pair[0]));
+        
+        var cnt = document.createElement("div");
+        cnt.innerHTML = pair[1];
+        cnt.className = "cnt";
+
+        dd.appendChild(div);
+        dd.appendChild(cnt);
+        return dd;
+    },
+    
     createIngredElement: function(item, bottle, name, last) {
         var dd = document.createElement("dd");
         if(last) dd.className = "last";
