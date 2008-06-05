9a8eab70977e294eb88b1084ff4d096a344eb95b
Peter Leonov <kungfutzu@programica.ru>
2008-06-05 08:38:45 +0000

    ~ some css minify + preloader, fixes #160

--- htdocs/js/compiled/cocktails.js
+++ htdocs/js/compiled/cocktails.js
@@ -3,6 +3,7 @@
 <!--#include file="/js/cocktails/controller.js" -->
 
 $.onload(function(){
+	$('preloader').hide()
 	new Programica.RollingImagesLite($('results_display'));
 	Controller.init();
 	Calculator.init();
