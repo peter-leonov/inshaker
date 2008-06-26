1030e1717572e1b32e2871ced2709b7c404c2cf4
Vasily Mikhailitchenko <vaskas@programica.ru>
2008-06-26 15:03:51 +0000

    revision

--- htdocs/js/common/svn.js
+++ htdocs/js/common/svn.js
@@ -1,7 +1,3 @@
-var _svnReg = new RegExp(/\$Revision:\ (\d+)\ \$/)
-
 var svn = {};
 svn.text = "$Revision$";
-svn.getRevision = function(){
-	return this.text.match(_svnReg)[1];
-}
+svn.revision = this.text.match(/\$Revision:\ (\d+)\ \$/)[1]
