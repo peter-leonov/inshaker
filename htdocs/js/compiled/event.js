bb7e077ed0e4648b119cdefd2cc817c9353c3c90
Peter Leonov <kungfutzu@programica.ru>
2008-08-12 17:40:55 +0000

    ~ event beta

--- htdocs/js/compiled/event.js
+++ htdocs/js/compiled/event.js
@@ -1,1 +1,9 @@
 window.addEventListener('keypress', function (e) { if (e.charCode == 109) $('model').toggle() }, false)
+
+$.onload
+(
+	function ()
+	{
+		cssQuery('.programica-rolling-images').forEach(function (v) { new Programica.RollingImagesLite(v, {animationType: 'easeOutQuad'}) })
+	}
+)
