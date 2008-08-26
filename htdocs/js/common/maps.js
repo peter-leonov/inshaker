d785df597a613f9eae3abef08c97b2c956a79826
Peter Leonov <kungfutzu@programica.ru>
2008-08-26 16:31:05 +0000

    + api key for mike

--- htdocs/js/common/maps.js
+++ htdocs/js/common/maps.js
@@ -5,6 +5,7 @@ function loadGoogleApi ()
 	{
 		'inshaker.petr.programica.ru': 'ABQIAAAARQzYWu9IpurDSJW9DIJqrxSXvIsyqgyb_kGywp9iIuSiaeA-JBQzTsDzLup3dwyBVfsiow2oek-qcQ',
 		'inshaker.vaskas.programica.ru': 'ABQIAAAARQzYWu9IpurDSJW9DIJqrxQREQYsNTRM9xy8NZxnUU8bL0VtnhSjjuhT-zGZ42oWWqrhR9EXDzXnNw',
+		'inshaker.mike.programica.ru': 'ABQIAAAARQzYWu9IpurDSJW9DIJqrxRNoEmkHlR0LBJ-kuMLS1qT8KG6DhQZlE7qvqGjzsnNOCNXY-Q4tLeKZg',
 		'www.inshaker.ru': 'ABQIAAAARQzYWu9IpurDSJW9DIJqrxRWeW9DvTiqRhftHfTMDG_ph9VZURSEW04TAvZRTH8g9VZCBYmnFl9AzA'
 	}
 	
