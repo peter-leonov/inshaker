fcb92299f6d82dfa8ad040686707d6daa7bde4aa
Vasily Mikhailitchenko <vaskas@programica.ru>
2008-09-15 10:36:56 +0000

    minor cleanup

--- htdocs/js/compiled/parties.js
+++
@@ -1,5 +0,0 @@
-<!--# include file="/js/parties/parties.js" -->
-<!--# include file="/js/parties/model.js" -->
-<!--# include file="/js/parties/controller.js" -->
-<!--# include file="/js/parties/view.js" -->
-
