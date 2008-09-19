c8ac3a04246b24333eabe0a59e22932ce4d1098d
Peter Leonov <kungfutzu@programica.ru>
2008-09-19 14:29:46 +0000

    + maps api for barman

--- htdocs/js/common/maps.js
+++ htdocs/js/common/maps.js
@@ -6,6 +6,7 @@ function loadGoogleApi ()
 		'inshaker.petr.programica.ru': 'ABQIAAAARQzYWu9IpurDSJW9DIJqrxSXvIsyqgyb_kGywp9iIuSiaeA-JBQzTsDzLup3dwyBVfsiow2oek-qcQ',
 		'inshaker.vaskas.programica.ru': 'ABQIAAAARQzYWu9IpurDSJW9DIJqrxQREQYsNTRM9xy8NZxnUU8bL0VtnhSjjuhT-zGZ42oWWqrhR9EXDzXnNw',
 		'inshaker.mike.programica.ru': 'ABQIAAAARQzYWu9IpurDSJW9DIJqrxRNoEmkHlR0LBJ-kuMLS1qT8KG6DhQZlE7qvqGjzsnNOCNXY-Q4tLeKZg',
+		'inshaker.barman.programica.ru': 'ABQIAAAARQzYWu9IpurDSJW9DIJqrxRoSFFn0eFsI-JMwoV5H95olICkthS-DWVvrBtomxsSInR3XbVM9Kkx7Q',
 		'www.inshaker.ru': 'ABQIAAAARQzYWu9IpurDSJW9DIJqrxRWeW9DvTiqRhftHfTMDG_ph9VZURSEW04TAvZRTH8g9VZCBYmnFl9AzA'
 	}
 	
