f0c16af3277bf4ee3685c5c376e9e53c254545c1
Vasily Mikhailitchenko <vaskas@programica.ru>
2009-03-25 10:15:04 +0000

    updated decorartions processor and templates

--- barman/processors/decoration.rb
+++ barman/processors/decoration.rb
@@ -13,16 +13,21 @@ class DecorationProcessor < Barman::Processor
   end
   
   def run
+    @theme = ARGV[0] || YAML::load(File.open(Config::DECORATION_DIR + "decoration.yaml"))['Выбранная тема'] 
+    @theme_dir  = Config::DECORATION_DIR + @theme + "/"
+    
     @params = {}
-    @params[:links]       = YAML::load(File.open(Config::DECORATION_DIR + "Links/links.yaml"))['Inshaker рекомендует']
-    @params[:promos]      = YAML::load(File.open(Config::DECORATION_DIR + "Promos/promos.yaml"))['Промо']
-    @params[:cocktails]   = YAML::load(File.open(Config::DECORATION_DIR + "cocktails.yaml"))['Коктейли']
-    @params[:spotlighted] = YAML::load(File.open(Config::DECORATION_DIR + "Spotlighted/spotlighted.yaml"))['В центре внимания'][0]
+    @params[:links]       = YAML::load(File.open(@theme_dir + "Links/links.yaml"))['Inshaker рекомендует']
+    @params[:promos]      = YAML::load(File.open(@theme_dir + "Promos/promos.yaml"))['Промо']
+    @params[:cocktails]   = YAML::load(File.open(@theme_dir + "cocktails.yaml"))['Коктейли']
+    @params[:spotlighted] = YAML::load(File.open(@theme_dir + "Spotlighted/spotlighted.yaml"))['В центре внимания'][0]
     
     flush_links_and_promo
-    flush_backgrounds
     flush_spotlighted
     flush_json
+    flush_theme_files
+    flush_backgrounds
+    #debug
   end
 
   def debug
@@ -33,10 +38,9 @@ class DecorationProcessor < Barman::Processor
     to_copy = ["Links", "Promos"]
     
     to_copy.each do |dir|
-      src_dir  = Config::DECORATION_DIR + dir
+      src_dir  = @theme_dir + dir
       dest_dir = Config::HTDOCS_DIR + "i/index/" + dir.downcase
 
-      FileUtils.rm_rf dest_dir
       FileUtils.mkdir_p dest_dir
     
       Dir.new(src_dir).each do |f|
@@ -48,12 +52,12 @@ class DecorationProcessor < Barman::Processor
   end
   
   def flush_spotlighted
-    FileUtils.cp_r "#{Config::DECORATION_DIR}/Spotlighted/icon.png", "#{Config::HTDOCS_DIR}/t/icon/spotlighted.png", @mv_opt
+    FileUtils.cp_r "#{@theme_dir}/Spotlighted/icon.png", "#{Config::HTDOCS_DIR}/t/icon/spotlighted.png", @mv_opt
   end
 
   def flush_backgrounds
-    backs = Config::DECORATION_DIR + "Backgrounds"
-    FileUtils.rm_rf Config::THEME_FILE
+    backs = @theme_dir + "Backgrounds"
+    #FileUtils.rm_rf Config::THEME_FILE
 
     Dir.new(backs).each do |f|
       if f=~ /(\w+)\.(png|jpg)$/
@@ -61,10 +65,10 @@ class DecorationProcessor < Barman::Processor
         FileUtils.mkdir_p out_dir
         FileUtils.cp_r "#{backs}/#{f}", "#{out_dir}/bg.#{$2}", @mv_opt
         
-        File.open(Config::THEME_FILE, "a+") do |theme|
-          theme.puts get_theme_entry($1, $2)
-          theme.close
-        end
+        #File.open(Config::THEME_FILE, "a+") do |theme|
+        #  theme.puts get_theme_entry($1, $2)
+        #  theme.close
+        #end
 
       end
     end
@@ -74,10 +78,15 @@ class DecorationProcessor < Barman::Processor
     flush_json_object(@params, Config::DB_JS)
   end
 
+  def flush_theme_files
+    puts "Selected theme: #{@theme}"
+    FileUtils.cp_r "#{@theme_dir}/UI/.", Config::HTDOCS_DIR
+  end
+
 private 
   
   def get_theme_entry page, bg_ext
-    return "##{page}-page, body.#{page}-page {background:transparent url(bg/#{page}/bg.#{bg_ext}) repeat-x -139px -141px;} "
+    return "##{page}-page body, .#{page}-page body { background-image:url(bg/#{page}/bg.#{bg_ext}) } "
   end
   
 end
