a8962e31783c5960f9b7fc92ffa8fd78f5f04a86
Peter Leonov <kungfutzu@programica.ru>
2009-07-17 16:26:43 +0400

    indentation tweaks

--- htdocs/js/index/model.js
+++ htdocs/js/index/model.js
@@ -7,17 +7,19 @@ function IndexPageModel ()
 
 IndexPageModel.prototype =
 {
-	initialize: function (params) 
-  {
-    for(var key in params) this[key] = params[key]
-  },
+	initialize: function (params)
+	{
+		for(var key in params)
+			this[key] = params[key]
+	},
 	
 	setState: function (state)
 	{
 		var data =
 		{
 			cocktails: this.cocktails.map(function (v) { return Cocktail.getByName(v) }),
-            links: this.links, promos: this.promos
+			links: this.links,
+			promos: this.promos
 		}
 		
 		this.view.modelChanged(data, state)
