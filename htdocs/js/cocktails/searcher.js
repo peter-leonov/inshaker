5eeed3b564369737726ead929d850abe92d28624
Peter Leonov <kungfutzu@programica.ru>
2009-07-29 18:17:27 +0400

    IngredientsSearcher now supports alternative names

--- htdocs/js/cocktails/searcher.js
+++ htdocs/js/cocktails/searcher.js
@@ -6,10 +6,10 @@ eval(NodesShortcut())
 
 Me.prototype.extend
 ({
-	initialize: function ()
+	initialize: function (ingredients, names)
 	{
-		this.ingredients = []
-		this.names = []
+		this.ingredients = ingredients || []
+		this.names = names || {}
 		this.cache = {}
 	},
 	
@@ -23,20 +23,20 @@ Me.prototype.extend
 		else
 		{
 			if (!(res = cache[substr]))
-				res = cache[substr] = this.searchInSet(this.ingredients, substr, count)
+				res = cache[substr] = this.searchInSet(this.ingredients, this.names, substr, count)
 		}
 		
 		return res
 	},
 	
-	searchInSet: function (set, substr, count)
+	searchInSet: function (set, names, substr, count)
 	{
 		var rex = new RegExp('(^|.*\\s)(' + substr + ')(.*)', 'i'),
 			res = []
 		
 		for (var i = 0, il = set.length; i < il && count > 0; i++)
 		{
-			var m, v = set[i]
+			var m, name, v = set[i]
 			if (m = rex.exec(v))
 			{
 				var text = N('span')
@@ -44,6 +44,12 @@ Me.prototype.extend
 				// m[2] is used instead of substr because m[2] != substr when searching with "i"
 				text.appendChild(N('span', 'substr', m[2]))
 				text.appendChild(T(m[3]))
+				var name = names[v]
+				if (name)
+				{
+					text.appendChild(T(' — это «' + name + '»'))
+					v = name
+				}
 				res.push([v, text])
 				count--
 			}
