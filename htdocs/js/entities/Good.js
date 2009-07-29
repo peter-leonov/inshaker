b91d21c276b1337db8bc3f16863888ef837d0ce3
Peter Leonov <kungfutzu@programica.ru>
2009-07-29 17:08:51 +0400

    Good entity added (finds out the names)

--- htdocs/js/entities/Good.js
+++ htdocs/js/entities/Good.js
@@ -1,2 +1,26 @@
 // TODO: Wrap it in those cute objects like you did with other entities
 var goods = <!--# include file="/db/goods.js" -->;
+
+Good = function () {}
+
+Object.extend(Good,
+{
+	initialize: function (db)
+	{
+		var anames = this.names = {}
+		
+		for (var k in db)
+		{
+			var good = db[k],
+				names = good.names
+				
+			if (names)
+				for (var i = 0; i < names.length; i++)
+					anames[names[i]] = k
+		}
+		
+		log(anames)
+	}
+})
+
+Good.initialize(goods)
