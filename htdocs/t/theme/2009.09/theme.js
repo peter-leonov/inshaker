3ce1f8e21f42543842edce1a0c5da475734bae8a
Peter Leonov <kungfutzu@programica.ru>
2009-09-02 19:22:59 +0400

    new link for Olmeca

--- htdocs/t/theme/2009.09/theme.js
+++ htdocs/t/theme/2009.09/theme.js
@@ -1,4 +1,4 @@
 {
-	"spotlighted":{"href":"/cocktails.html#state=byIngredients&ingredients=%D0%97%D0%BE%D0%BB%D0%BE%D1%82%D0%B0%D1%8F%20%D1%82%D0%B5%D0%BA%D0%B8%D0%BB%D0%B0%2C%D0%A1%D0%B5%D1%80%D0%B5%D0%B1%D1%80%D1%8F%D0%BD%D0%B0%D1%8F%20%D1%82%D0%B5%D0%BA%D0%B8%D0%BB%D0%B0"},
-	"branded-image":{"href":"/cocktails.html#state=byIngredients&ingredients=%D0%97%D0%BE%D0%BB%D0%BE%D1%82%D0%B0%D1%8F%20%D1%82%D0%B5%D0%BA%D0%B8%D0%BB%D0%B0%2C%D0%A1%D0%B5%D1%80%D0%B5%D0%B1%D1%80%D1%8F%D0%BD%D0%B0%D1%8F%20%D1%82%D0%B5%D0%BA%D0%B8%D0%BB%D0%B0"}
+	"spotlighted":{"href":"/cocktails.html#state=byIngredients&ingredients=%D0%97%D0%BE%D0%BB%D0%BE%D1%82%D0%B0%D1%8F%20%D1%82%D0%B5%D0%BA%D0%B8%D0%BB%D0%B0%2C%D0%A1%D0%B5%D1%80%D0%B5%D0%B1%D1%80%D1%8F%D0%BD%D0%B0%D1%8F%20%D1%82%D0%B5%D0%BA%D0%B8%D0%BB%D0%B0&tag=%D0%BD%D0%B0%20%D0%BA%D0%BE%D0%BC%D0%BF%D0%B0%D0%BD%D0%B8%D1%8E"},
+	"branded-image":{"href":"/cocktails.html#state=byIngredients&ingredients=%D0%97%D0%BE%D0%BB%D0%BE%D1%82%D0%B0%D1%8F%20%D1%82%D0%B5%D0%BA%D0%B8%D0%BB%D0%B0%2C%D0%A1%D0%B5%D1%80%D0%B5%D0%B1%D1%80%D1%8F%D0%BD%D0%B0%D1%8F%20%D1%82%D0%B5%D0%BA%D0%B8%D0%BB%D0%B0&tag=%D0%BD%D0%B0%20%D0%BA%D0%BE%D0%BC%D0%BF%D0%B0%D0%BD%D0%B8%D1%8E"}
 }
