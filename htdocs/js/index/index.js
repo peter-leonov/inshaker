83597a1bb36389623efa2d3efdd572d51c8e7aa3
Peter Leonov <kungfutzu@programica.ru>
2009-09-21 19:42:41 +0400

    follow us on twitter (closes #617)

--- htdocs/js/index/index.js
+++ htdocs/js/index/index.js
@@ -2,8 +2,8 @@ IndexPage =
 {
 	initialize: function (nodes)
 	{
-        var params = <!--# include file="/db/magazine.js"-->
-        var model       = this.model         = new IndexPageModel(params)
+		var params = <!--# include file="/db/magazine.js"-->
+		var model       = this.model         = new IndexPageModel(params)
 		var controller  = this.controller    = new IndexPageController()
 		var view        = this.view          = new IndexPageView(nodes)
 		
@@ -24,25 +24,14 @@ $.onload
 		{
 			cocktails: cssQuery('.info-blocks .cocktails-list')[0],
 			links: cssQuery('.info-blocks .links-list')[0],
-            promo: $('promo'),
-			dontMiss: $('dont-miss'),
-            arrows:[cssQuery('#promo-prev')[0], cssQuery('#promo-next')[0]]
+			promo: $('promo'),
+			arrows:[cssQuery('#promo-prev')[0], cssQuery('#promo-next')[0]]
 		}
-
-    IndexPage.initialize(nodes)
+		
+		IndexPage.initialize(nodes)
 	}
 )
-
-<!--# include file="/lib/Programica/Request.js" -->
-<!--# include file="/lib/Programica/Form.js" -->
-
-<!--# include file="/lib/Programica/Widget.js" -->
-<!--# include file="/lib/Widgets/FormPoster.js" -->
-
-<!--# include file="/js/common/autocompleter.js" -->
-<!--# include file="/js/common/newsFormPopup.js" -->
 
 <!--# include file="/js/index/model.js" -->
 <!--# include file="/js/index/controller.js" -->
 <!--# include file="/js/index/view.js" -->
-
