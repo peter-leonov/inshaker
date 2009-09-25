ad30c1041fec5e3b369d9a0c0c0640c0299402d1
Peter Leonov <kungfutzu@programica.ru>
2009-09-25 14:54:49 +0400

    pulling deployer (closes #524)

--- barman/deployer.rb
+++ barman/deployer.rb
@@ -7,13 +7,17 @@ class Deployer < Barman::Processor
   end
   
   def job
-    unless system("cd #{Config::BASE_DIR} && git commit1 -am 'content update' 2>&1")
-      error "не могу сохранить обновления в гит"
+    unless system("cd #{Config::BASE_DIR} && git pull 2>&1")
+      error "не удалось синхронизироваться с сайтом"
     else
-      unless system("cd #{Config::BASE_DIR} && git push1 2>&1")
-        error "не могу залить обновления на сайт"
+      unless system("cd #{Config::BASE_DIR} && git commit -am 'content update' 2>&1")
+        error "не удалось сохранить обновления в гит"
       else
-        say "все сохранил и залил на сайт"
+        unless system("cd #{Config::BASE_DIR} && git push 2>&1")
+          error "не удалось залить обновления на сайт"
+        else
+          say "все сохранил и залил на сайт"
+        end
       end
     end
   end
