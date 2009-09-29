2b116bf066833a3974d21df28a56b6694e67599b
Peter Leonov <kungfutzu@programica.ru>
2009-09-29 14:55:02 +0400

    cocktails common cut out from compiled common

---
+++ htdocs/js/common/cocktails.js
@@ -0,0 +1,12 @@
+<!--# include file="/js/common/good_helper.js" -->
+<!--# include file="/js/common/datafilter.js" -->
+<!--# include file="/js/common/storage.js" -->
+<!--# include file="/js/common/dnd.js" -->
+<!--# include file="/js/common/theme.js" -->
+
+<!--# include file="/js/calculator/util.js" -->
+<!--# include file="/js/calculator/model.js" -->
+<!--# include file="/js/calculator/view.js" -->
+<!--# include file="/js/calculator/controller.js" -->
+<!--# include file="/js/calculator/calculator.js" -->
+
