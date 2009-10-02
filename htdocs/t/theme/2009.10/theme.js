7c77b600a4761d3800b58ec06f6789cadae3b269
Barman <barman@inshaker.ru>
2009-10-02 13:56:13 +0000

    content update

--- htdocs/t/theme/2009.10/theme.js
+++ htdocs/t/theme/2009.10/theme.js
@@ -1,4 +1,4 @@
 {
-	"spotlighted":{"href":"/cocktails.html#tag=%D0%B0%D0%B1%D1%81%D0%BE%D0%BB%D1%8E%D1%82%D0%BD%D0%B0%D1%8F%20%D0%BE%D1%81%D0%B5%D0%BD%D1%8C&state=byIngredients"},
-	"branded-image":{"href":"/cocktails.html#tag=%D0%B0%D0%B1%D1%81%D0%BE%D0%BB%D1%8E%D1%82%D0%BD%D0%B0%D1%8F%20%D0%BE%D1%81%D0%B5%D0%BD%D1%8C&state=byIngredients"}
+	"spotlighted":{"href":"/cocktails.html#tag=%D1%81%D0%B5%D0%B7%D0%BE%D0%BD%D0%BD%D1%8B%D0%B5&state=byIngredients"},
+	"branded-image":{"href":"/cocktails.html#tag=%D1%81%D0%B5%D0%B7%D0%BE%D0%BD%D0%BD%D1%8B%D0%B5&state=byIngredients"}
 }
