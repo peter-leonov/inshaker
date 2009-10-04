91bf8ef1932e321729ccbe86caa1176c10902730
Peter Leonov <kungfutzu@programica.ru>
2009-10-04 17:10:03 +0400

    horrible relaatedCocktails() deleted

--- htdocs/js/common/datafilter.js
+++ htdocs/js/common/datafilter.js
@@ -143,37 +143,6 @@ self.DataFilter = {
 		return res;
 	},
 	
-	relatedCocktails: function(set, cocktail, howMany) {
-		var res = [];
-		var ingreds = [];
-		var possibleSets = [];
-		
-		// for(var i = 0; i < cocktail.ingredients.length; i++) {
-			ingreds.push(cocktail.ingredients[0][0]);
-		// }
-		var ingredsCopy = cloneObject(ingreds);
-		possibleSets.push([].concat(ingreds));
-		
-		while(ingreds.length > 1) {
-			var spliced = ingreds.length-1;
-			while(spliced >= 0){
-				possibleSets.push(ingreds.without(spliced));
-				spliced--;
-			}
-			ingreds.splice(ingreds.length-1, 1);
-		}
-		
-		for(var i = 0; i < ingredsCopy.length; i++) possibleSets.push([].concat(ingredsCopy[i]));
-		
-		for(var i = 0; i < possibleSets.length; i++){
-			var cocktails = this.cocktailsByIngredients(set, possibleSets[i]);
-			cocktails.splice(cocktails.indexOf(cocktail), 1);
-			res = res.concat(cocktails.sort(this.nameSort));
-			if(res.uniq().length >= howMany) break;
-		}
-		return res.uniq().slice(0, howMany);
-	},
-	
 	ingredientsByLetter: function(set, letter){
 		var res = [];	
 		var reg = new RegExp("^(" + letter.toUpperCase() + ")");
