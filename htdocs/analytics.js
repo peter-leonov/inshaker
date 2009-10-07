a7a10a0bd6a26895535a83ca11ef277fc08d58ad
Peter Leonov <kungfutzu@programica.ru>
2009-10-07 20:23:13 +0400

    analytics done

--- htdocs/analytics.js
+++ htdocs/analytics.js
@@ -1,3 +1,4 @@
+<!--# include file="/lib/Programica/Form.js" -->
 <!--# include file="/js/common/class.js" -->
 <!--# include file="/js/common/url-encode.js" -->
 $$ = cssQuery
@@ -28,7 +29,8 @@ Me.prototype.extend
 		var me = this
 		nodes.login.addEventListener('click', function (e) { me.doLogin() }, false)
 		nodes.logout.addEventListener('click', function (e) { me.doLogout() }, false)
-		nodes.calculate.addEventListener('click', function (e) { me.doCalculate() }, false)
+		nodes.ingredientForm.addEventListener('submit', function (e) { e.preventDefault(); me.doCalculate(e.target.toHash()) }, false)
+		
 		this.handleErrorCallback = function (err) { me.handleError(err) }
 		this.handleAccountFeedCallback = function (r) { me.handleAccountFeed(r) }
 		return this
@@ -49,8 +51,6 @@ Me.prototype.extend
 			this.nodes.main.addClassName('logged-in')
 			this.loadData()
 		}
-		
-		this.nodes.html.removeClassName('loading')
 	},
 	
 	doLogin: function ()
@@ -62,10 +62,6 @@ Me.prototype.extend
 	{
 		this.api.accounts.user.logout()
 		location.href = location.href
-	},
-	
-	doCalculate: function ()
-	{
 	},
 	
 	loadData: function ()
@@ -96,7 +92,7 @@ Me.prototype.extend
 					'metrics': 'ga:pageviews',
 					'filters': 'ga:pagePath=@/cocktails/',
 					// 'sort':'-ga:pageviews',
-					'max-results': 10,
+					'max-results': 1000,
 					'ids': 'ga:' + id
 				}
 			
@@ -106,7 +102,9 @@ Me.prototype.extend
 		var substitute =
 		{
 			'/cocktails/---_on_the_beach.html': '/cocktails/bitch_on_the_beach.html',
-			'Inshaker — ---- на пляже': 'Inshaker — Сука на пляже'
+			'Inshaker — ---- на пляже': 'Inshaker — Сука на пляже',
+			'/cocktails/safe_---_on_the_beach.html': '/cocktails/safe_sex_on_the_beach.html',
+			'Inshaker — Безопасный ---- на пляже': 'Inshaker — Безопасный секс на пляже'
 		}
 		
 		function dxpToHash (dxp, hash)
@@ -136,14 +134,62 @@ Me.prototype.extend
 				dxpToHash(entry.dxp$dimension, hash)
 				dxpToHash(entry.dxp$metric, hash)
 				
-				log(hash)
+				// log(hash)
 				
 				var path = hash['ga:pagePath']
 				pageviews[path] = {path: path, title: hash['ga:pageTitle'], views: hash['ga:pageviews']}
 			}
 			
-			log(Object.stringify(pageviews))
+			// log(Object.stringify(pageviews))
 		}
+	},
+	
+	dataReady: function ()
+	{
+		this.nodes.html.removeClassName('loading')
+	},
+	
+	doCalculate: function (form)
+	{
+		var cocktails = Cocktail.getByIngredients(form.ingredients.split(/\s*,\s*/)),
+			pageviews = this.pageviews, output = this.nodes.output
+		
+		this.clear()
+		var sum = 0
+		for (var i = 0; i < cocktails.length; i++)
+		{
+			var cocktail = cocktails[i],
+				uri = cocktail.name_eng.htmlName(),
+				data = pageviews['/cocktails/' + uri + '.html']
+			
+			if (!data)
+				this.error('Нет статистики для ' + cocktail.name + ', по адресу ' + uri)
+			else
+			{
+				this.print(cocktail.name + ', ' + data.views)
+				sum += +data.views
+			}
+		}
+		
+		this.print('————————————————————————————————————————')
+		this.print('Всего просмотров: ' + sum)
+	},
+	
+	clear: function ()
+	{
+		this.nodes.output.empty()
+	},
+	
+	print: function (str)
+	{
+		this.nodes.output.appendChild(document.createElement('li')).appendChild(document.createTextNode(str))
+	},
+	
+	error: function (str)
+	{
+		var li = this.nodes.output.appendChild(document.createElement('li'))
+		li.className = 'error'
+		li.appendChild(document.createTextNode(str))
 	}
 })
 
@@ -157,7 +203,8 @@ function onready ()
 		main: $$('#analytics')[0],
 		login: $$('#analytics #login')[0],
 		logout: $$('#analytics #logout')[0],
-		calculate: $$('#analytics #calculate')[0]
+		ingredientForm: $$('#analytics #ingredient-search')[0],
+		output: $$('#analytics #output')[0]
 	}
 	
 	var conf =
@@ -166,6 +213,8 @@ function onready ()
 	}
 	
 	var an = new Analytics().bind(nodes, conf)
-	google.load('gdata', '1.x', {packages: ['analytics'], callback: function () { an.apiLoaded(google) }})
+	// google.load('gdata', '1.x', {packages: ['analytics'], callback: function () { an.apiLoaded(google) }})
+	an.pageviews = <!--# include file="anal.js" -->
+	an.dataReady()
 }
 $.onready(onready)
