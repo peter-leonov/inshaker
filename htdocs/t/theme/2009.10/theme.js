273c4871af495c4c969744b7bc37d6d54d719e5a
Peter Leonov <kungfutzu@programica.ru>
2009-10-30 17:55:17 +0300

    release

--- htdocs/t/theme/2009.10/theme.js
+++ htdocs/t/theme/2009.10/theme.js
@@ -1,4 +1,4 @@
 {
-	"spotlighted":{"href":"/cocktails.html#tag=%D1%81%D0%B5%D0%B7%D0%BE%D0%BD%D0%BD%D1%8B%D0%B5&state=byIngredients"},
-	"branded-image":{"href":"/cocktails.html#tag=%D1%81%D0%B5%D0%B7%D0%BE%D0%BD%D0%BD%D1%8B%D0%B5&state=byIngredients"}
+	"spotlighted":{"href":"/cocktails.html#state=byName&name=%D0%A1%D0%B0%D0%BC%D0%B1%D1%83%D0%BA%D0%B0"},
+	"branded-image":{"href":"/cocktails.html#state=byName&name=%D0%A1%D0%B0%D0%BC%D0%B1%D1%83%D0%BA%D0%B0"}
 }
