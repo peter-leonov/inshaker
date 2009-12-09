6f1df5ead10bcef6e0d9ecbaa937725b35d283c2
Peter Leonov <gojpeg@gmail.com>
2009-12-09 16:58:31 +0300

    0.3 in parallel

---
+++ htdocs/js/common/common-0.3.js
@@ -0,0 +1,2 @@
+<!--# include file="/js/common/programica-0.3.js" -->
+<!--# include file="/js/common/analytics.js" -->
---
+++ htdocs/js/common/programica-0.3.js
@@ -0,0 +1,7 @@
+<!--# include virtual="/lib-0.3/programica/init.js" -->
+<!--# include virtual="/lib-0.3/programica/prototype.js" -->
+<!--# include virtual="/lib-0.3/programica/class.js" -->
+
+<!--# include virtual="/lib-0.3/modules/mvc.js" -->
+<!--# include virtual="/lib-0.3/modules/cosy.js" -->
+<!--# include virtual="/lib-0.3/modules/onready.js" -->
