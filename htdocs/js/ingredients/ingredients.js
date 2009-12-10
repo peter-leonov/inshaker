cdf7208e7eb5f3c6426aa56b840814c42d765820
Peter Leonov <gojpeg@gmail.com>
2009-12-10 04:39:27 +0300

    baking a skeleton

--- htdocs/js/ingredients/ingredients.js
+++ htdocs/js/ingredients/ingredients.js
@@ -1,9 +1,38 @@
 ;(function(){
 
-var myName = 'IngredientsPage',
+var myName = 'WidgetName',
 	Me = self[myName] = MVC.create(myName)
 
 // Me.mixIn(EventDriven)
+
+var myProto =
+{
+	initialize: function ()
+	{
+		this.model.initialize()
+		this.view.initialize()
+		this.controller.initialize()
+	},
+
+	bind: function (nodes, sources, state)
+	{
+		this.model.bind(sources)
+		this.view.bind(nodes)
+		this.controller.bind(state)
+		
+		return this
+	}
+}
+
+Object.extend(Me.prototype, myProto)
+
+})();
+
+
+;(function(){
+
+var Papa = WidgetName, Me = Papa.View
+
 // eval(NodesShortcut())
 
 var myProto =
@@ -19,6 +48,50 @@ var myProto =
 		
 		
 		return this
+	},
+	
+	modelChanged: function (data)
+	{
+		
+	}
+}
+
+Object.extend(Me.prototype, myProto)
+
+})();
+
+
+;(function(){
+
+var Papa = WidgetName, Me = Papa.Controller
+
+var myProto =
+{
+	initialize: function ()
+	{
+		this.state = {}
+	},
+	
+	bind: function (state)
+	{
+		this.state = state
+	}
+}
+
+Object.extend(Me.prototype, myProto)
+
+})();
+
+
+;(function(){
+
+var Papa = WidgetName, Me = Papa.Model
+
+var myProto =
+{
+	initialize: function ()
+	{
+		this.sources = {}
 	}
 }
 
