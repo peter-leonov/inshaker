b81c14a90c095d9d6100ee4b265884f1ef302fb2
Peter Leonov <gojpeg@gmail.com>
2009-12-10 20:15:49 +0300

    added some more “common” code

--- htdocs/js/common/programica-0.3.js
+++ htdocs/js/common/programica-0.3.js
@@ -2,6 +2,10 @@
 <!--# include virtual="/lib-0.3/programica/prototype.js" -->
 <!--# include virtual="/lib-0.3/programica/class.js" -->
 
+<!--# include virtual="/lib-0.3/modules/log.js" -->
 <!--# include virtual="/lib-0.3/modules/mvc.js" -->
 <!--# include virtual="/lib-0.3/modules/cosy.js" -->
+<!--# include virtual="/lib-0.3/modules/selectors.js" -->
 <!--# include virtual="/lib-0.3/modules/onready.js" -->
+<!--# include virtual="/lib-0.3/modules/element.js" -->
+<!--# include virtual="/lib-0.3/modules/nodes.js" -->
