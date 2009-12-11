b41b598e0cf29c743583e26e91e105155a3d8454
Peter Leonov <gojpeg@gmail.com>
2009-12-11 03:56:46 +0300

    removing global goods and Good part II

--- htdocs/js/cocktail/model.js
+++ htdocs/js/cocktail/model.js
@@ -1,7 +1,7 @@
 var Model = {
 	cocktail: null,
 	ingredients: [],
-	goods: goods,
+	goods: Ingredient.getAll(),
 	
 	dataListener: null,
 	
@@ -21,7 +21,7 @@ var Model = {
 		var ingreds = cocktail.ingredients; 
 		
 		for(var i = 0; i < ingreds.length; i++){
-			var items = goods[ingreds[i][0]];
+			var items = this.goods[ingreds[i][0]];
 			if(items && items.mark && this._doesntHave(recs, items.mark)){
 				var rec = {};
 				rec.mark  = items.mark;
