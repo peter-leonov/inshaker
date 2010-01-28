b8caeb8d8f81c5492c8aa8e0a1275815b915855d
Peter Leonov <gojpeg@gmail.com>
2010-01-28 16:17:17 +0300

    rewriting the matching search and porting it to the 0.3

--- htdocs/js/matching/model.js
+++ htdocs/js/matching/model.js
@@ -1,53 +1,30 @@
-var Model =
+;(function(){
+
+var Papa = MatchingPage, Me = Papa.Model
+
+var myProto =
 {
-	dataListener: null,
-	
-	init: function (listener)
+	initialize: function ()
 	{
-		this.dataListener = listener
+		this.sources = {}
+		this.state = {}
+		this.data = {}
 	},
 	
-	uniqueLetters: function ()
+	bind: function (sources)
 	{
-		var names = Ingredient.getAllNames(),
-			seen = {}, res = []
-		
-		for (var i = 0, il = names.length; i < il; i++)
-		{
-			var letter = names[i].charAt(0)
-			if (!seen[letter])
-			{
-				res.push(letter)
-				seen[letter] = true
-			}
-		}
-		
-		return res
+		this.sources = sources
+		this.data.allIngredients = this.sources.ingredient.getAll()
 	},
 	
-	ingredientsOn: function (letter)
+	setState: function (state)
 	{
-		return Ingredient.getByFirstLetter(letter);
-	},
-	
-	suitableIngredients: function (list)
-	{
-		var res = [],
-			cocktails = Cocktail.getByIngredientNames(list)
+		this.state = state
 		
-		for (var i = 0; i < cocktails.length; i++)
-		{
-			var cocktail = cocktails[i]
-			for (var j = 0; j < cocktail.ingredients.length; j++)
-				res.push(cocktail.ingredients[j][0]) // [0] is an ingredient name
-		}
-		
-		return {ingredients: res.uniq(), cocktails: cocktails}
-	},
-	
-	selectedListChanged: function (selectedList)
-	{
-		var set = this.suitableIngredients(selectedList)
-		this.dataListener.updateCount(set.cocktails.length, set.ingredients, selectedList.length)
+		this.view.modelChanged({ingredients: this.data.allIngredients, selected: []})
 	}
 }
+
+Object.extend(Me.prototype, myProto)
+
+})();
