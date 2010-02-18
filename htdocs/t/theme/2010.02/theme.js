569256a9219a719d3fc8a09c29f1a0d9aee0cbcd
Peter Leonov <gojpeg@gmail.com>
2010-02-18 19:52:27 +0300

    bg, link and a few other updates

--- htdocs/t/theme/2010.02/theme.js
+++ htdocs/t/theme/2010.02/theme.js
@@ -1,4 +1,4 @@
 {
-	"spotlighted":{"href":"/cocktails.html#state=byIngredients&tag=absolut%20%D1%8D%D1%82%D0%BE%20%D0%BF%D1%80%D0%BE%D1%81%D1%82%D0%BE"},
-	"branded-image":{"href":"/cocktails.html#state=byIngredients&tag=absolut%20%D1%8D%D1%82%D0%BE%20%D0%BF%D1%80%D0%BE%D1%81%D1%82%D0%BE"}
+	"spotlighted":{"href":"/cocktails.html#state=byIngredients&ingredients=%D0%97%D0%BE%D0%BB%D0%BE%D1%82%D0%B0%D1%8F%20%D1%82%D0%B5%D0%BA%D0%B8%D0%BB%D0%B0%20100%25%20%D0%B0%D0%B3%D0%B0%D0%B2%D0%B0"},
+	"branded-image":{"href":"/cocktails.html#state=byIngredients&ingredients=%D0%97%D0%BE%D0%BB%D0%BE%D1%82%D0%B0%D1%8F%20%D1%82%D0%B5%D0%BA%D0%B8%D0%BB%D0%B0%20100%25%20%D0%B0%D0%B3%D0%B0%D0%B2%D0%B0"}
 }
