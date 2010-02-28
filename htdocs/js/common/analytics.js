352ac37b9732bd3e2877a0914369fbf6efc427a3
Peter Leonov <gojpeg@gmail.com>
2010-02-28 11:30:23 +0300

    migrated to the async ga.js API

--- htdocs/js/common/analytics.js
+++ htdocs/js/common/analytics.js
@@ -1,25 +1,31 @@
 ;(function(){
 
-var interval
+var account = 'UA-1635720-11'
+
+var _gaq = window._gaq
+if (!_gaq)
+	_gaq = window._gaq = []
+
+_gaq.push(function ()
+{
+	var tracker = window._gaq._createAsyncTracker(account, 'tracker')
+	tracker._trackPageview()
+})
+
+_gaq.push(function ()
+{
+	var tracker = window._gaq._getAsyncTracker('tracker')
+	tracker._trackPageview()
+})
+
+// async loading of async ga.js :)
 function load ()
 {
-	$.include(('https:' == document.location.protocol ? 'https://ssl.' : 'http://www.') + 'google-analytics.com/ga.js')
-	interval = setInterval(check, 500)
+	// untouched inclusion snippet
+	var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+	ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+	(document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(ga);
 }
-
-function check ()
-{
-	if (window._gat)
-	{
-		clearInterval(interval)
-		try
-		{
-			_gat._getTracker("UA-1635720-11")._trackPageview()
-		}
-		catch (ex) {}
-	}
-}
-
 $.onready(function () { setTimeout(function () { load() }, 250) })
 
 })();
