69e29f0d762183106b7608890213a93add1aa16b
Peter Leonov <gojpeg@gmail.com>
2010-03-05 19:35:47 +0300

    anti-yuppiebar is no more needed

--- htdocs/js/common/common.js
+++ htdocs/js/common/common.js
@@ -20,24 +20,3 @@ Oops.maybeEnable()
 
 String.prototype.htmlName = function () { return this.replace(/[^\w\-\.]/g, "_").toLowerCase() }
 String.prototype.capitalize = function () { return this.charAt(0).toUpperCase() + this.substr(1) }
-
-$.onready
-(
-	function ()
-	{
-		// anti-yuppiebar
-		function goTop () { window.parent.location = window.location }
-		function goDefend ()
-		{
-			try
-			{
-				var as = cssQuery('a')
-				for (var i = 0; i < as.length; i++)
-					as[i].target = '_top'
-			}
-			catch (ex) {}
-		}
-		if (window.parent !== window)
-			setTimeout(goDefend, 100)
-	}
-)
