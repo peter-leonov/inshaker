8f209fd6941829bd872484479a5f248009462fa4
Peter Leonov <gojpeg@gmail.com>
2010-03-05 09:47:29 +0300

    removes the separated programica file

--- htdocs/js/common/common.js
+++ htdocs/js/common/common.js
@@ -1,4 +1,15 @@
-<!--# include file="/js/common/programica.js" -->
+<!--# include file="/lib/Programica/Init.js" -->
+<!--# include file="/lib/Programica/Fixes.js" -->
+<!--# include file="/lib/Programica/OnReady.js" -->
+<!--# include file="/lib/Programica/DOM.js" -->
+<!--# include file="/lib/Programica/Animation.js" -->
+
+<!--# include file="/lib/Programica/UA.js" -->
+<!--# include file="/lib/Programica/Humanize.js" -->
+<!--# include file="/lib/Programica/Stringify.js" -->
+<!--# include file="/lib/Programica/Sizzle.js" -->
+<!--# include file="/lib/Widgets/RollingImagesLite.js" -->
+
 <!--# include file="/js/common/analytics.js" -->
 <!--# include file="/js/common/rutils.js" -->
 
