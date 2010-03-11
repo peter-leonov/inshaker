7d2d197b6c495e98387f17ac399d450513994ea2
Peter Leonov <gojpeg@gmail.com>
2010-03-11 11:42:19 +0300

    html stub and noscript links

--- barman/processors/goods.rb
+++ barman/processors/goods.rb
@@ -7,8 +7,10 @@ class GoodsProcessor < Barman::Processor
   module Config
     GOODS_DIR   = Barman::BASE_DIR + "Goods/"
     HTDOCS_DIR  = Barman::HTDOCS_DIR
+    ERB         = Barman::TEMPLATES_DIR + "good.rhtml"
     
-    HTDOCS_ROOT = HTDOCS_DIR + "good/"
+    HTDOCS_ROOT    = HTDOCS_DIR + "good/"
+    NOSCRIPT_LINKS = HTDOCS_ROOT + "links.html"
     
     DB_JS = HTDOCS_DIR + "db/goods.js"
   end
@@ -19,9 +21,14 @@ class GoodsProcessor < Barman::Processor
   
   def job
     @goods = []
-    @good  = {}
+    
+    prepare_dirs
+    prepare_renderer
+    
     process_goods
+    
     unless errors?
+      flush_links
       flush_json
     end
   end
@@ -30,35 +37,41 @@ class GoodsProcessor < Barman::Processor
     FileUtils.mkdir_p [Config::HTDOCS_ROOT]
   end
   
+  def prepare_renderer
+    @renderer = ERB.new(File.read(Config::ERB))
+  end
+  
   def process_goods
     root_dir = Dir.new(Config::GOODS_DIR)
     root_dir.each_dir do |good_dir|
       say good_dir.name
       indent do
       
-      @good = {}
+      good = {}
       yaml = load_yaml(good_dir.path + "/about.yaml")
       
-      @good["name"] = good_dir.name
-      @good["name_eng"] = yaml["По-английски"]
-      path = @good["path"] = @good["name_eng"].dirify
+      good["name"] = good_dir.name
+      good["name_eng"] = yaml["По-английски"]
+      path = good["path"] = good["name_eng"].dirify
       
-      @good["name_full"] = yaml["Полное название"]
-      @good["desc"] = yaml["Описание"].split("\n")
-      @good["places"] = yaml["Где купить"]
+      good["name_full"] = yaml["Полное название"]
+      good["desc"] = yaml["Описание"].to_s.gsub(/\n/, "<br/><br/>")
+      good["places"] = yaml["Где купить"]
       
       full_path = Config::HTDOCS_ROOT + '/' + path
       FileUtils.mkdir_p [full_path]
       dst_dir = Dir.new(full_path)
       
-      count = @good["images"] = update_images(good_dir, dst_dir)
+      count = good["images"] = update_images(good_dir, dst_dir)
       if count == 0
         error "не нашел ни одной большой картинки (начал искать с big-1.jpg)"
       else
         say "нашел #{count} #{count.plural("большую картинку", "большие картинки", "больших картинок")}"
       end
       
-      @goods << @good
+      File.write("#{dst_dir.path}/index.html", @renderer.result(GoodTemplate.new(good).get_binding))
+      
+      @goods << good
       end # indent
     end
     
@@ -90,6 +103,31 @@ class GoodsProcessor < Barman::Processor
     flush_json_object(@goods, Config::DB_JS)
   end
   
+  def flush_links
+    File.open(Config::NOSCRIPT_LINKS, "w+") do |links|
+      links.puts "<ul>"
+      @goods.each do |good|
+        links.puts %Q{<li><a href="/good/#{good["path"]}/">#{good["name"]}</a></li>}
+      end
+      links.puts "</ul>"
+    end
+  end
+  
 end
+
+class GoodTemplate
+  def initialize *hashes
+    hashes.each do |hash|
+      hash.each do |k, v|
+        instance_variable_set("@#{k}", v)
+      end
+    end
+  end
+  
+  def get_binding
+    binding
+  end
+end
+
 
 exit GoodsProcessor.new.run
