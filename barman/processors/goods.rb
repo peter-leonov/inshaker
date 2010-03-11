7202a9d170bd133f33d13ed1f126ed1827b1f6a0
Peter Leonov <gojpeg@gmail.com>
2010-03-11 11:11:36 +0300

    ht-path, images and clean json

--- barman/processors/goods.rb
+++ barman/processors/goods.rb
@@ -5,10 +5,10 @@ require 'barman'
 class GoodsProcessor < Barman::Processor
   
   module Config
-    GOODS_DIR  = Barman::BASE_DIR + "Goods/"
-    HTDOCS_DIR = Barman::HTDOCS_DIR
+    GOODS_DIR   = Barman::BASE_DIR + "Goods/"
+    HTDOCS_DIR  = Barman::HTDOCS_DIR
     
-    IMAGES_DIR = HTDOCS_DIR + "i/good/"
+    HTDOCS_ROOT = HTDOCS_DIR + "good/"
     
     DB_JS = HTDOCS_DIR + "db/goods.js"
   end
@@ -21,27 +21,39 @@ class GoodsProcessor < Barman::Processor
     @goods = []
     @good  = {}
     process_goods
-    flush_json
+    unless errors?
+      flush_json
+    end
+  end
+  
+  def prepare_dirs
+    FileUtils.mkdir_p [Config::HTDOCS_ROOT]
   end
   
   def process_goods
     root_dir = Dir.new(Config::GOODS_DIR)
-    
-    order = load_yaml(root_dir.path + "/order.yaml")
-    
     root_dir.each_dir do |good_dir|
       say good_dir.name
       indent do
+      
       @good = {}
       yaml = load_yaml(good_dir.path + "/about.yaml")
-      @good[:name] = good_dir.name
-      @good[:name_full] = yaml["Полное название"] 
-      @good[:desc] = yaml["Описание"].split("\n")
+      
+      @good["name"] = good_dir.name
+      @good["name_eng"] = yaml["По-английски"]
+      path = @good["path"] = @good["name_eng"].dirify
+      
+      @good["name_full"] = yaml["Полное название"]
+      @good["desc"] = yaml["Описание"].split("\n")
       @good["places"] = yaml["Где купить"]
       
-      count = @good["images"] = update_images(good_dir)
+      full_path = Config::HTDOCS_ROOT + '/' + path
+      FileUtils.mkdir_p [full_path]
+      dst_dir = Dir.new(full_path)
+      
+      count = @good["images"] = update_images(good_dir, dst_dir)
       if count == 0
-        error "не нашел ни одной большой картинки"
+        error "не нашел ни одной большой картинки (начал искать с big-1.jpg)"
       else
         say "нашел #{count} #{count.plural("большую картинку", "большие картинки", "больших картинок")}"
       end
@@ -50,18 +62,14 @@ class GoodsProcessor < Barman::Processor
       end # indent
     end
     
+    order = load_yaml(root_dir.path + "/order.yaml")
     @goods = @goods.sort { |a, b| (order.index(a[:name]) || 100) - (order.index(b[:name]) || 100) }
   end
   
-  def load_yaml filename
-    YAML::load(File.open(filename))
-  end
-  
-  def update_images good_dir
-    @good[:big_images] = []
+  def update_images src, dst
     counter = 0
-    while File.exists?(good_dir.path + "/big-#{counter+1}.jpg")
-      # @good[:big_images] << "/i/good/" + @good[:city].trans.html_name + "/" + @good[:name].trans.html_name + "-big-#{counter}.jpg"
+    while File.exists?(src.path + "/big-#{counter+1}.jpg")
+      cp_if_different src.path + "/big-#{counter+1}.jpg", dst.path + "/big-#{counter+1}.jpg"
       counter += 1
     end
     counter
@@ -69,6 +77,14 @@ class GoodsProcessor < Barman::Processor
   
   
   def flush_json
+    @goods.each do |good|
+      # YAGNI
+      good.delete("desc")
+      good.delete("places")
+      good.delete("name_full")
+      good.delete("images")
+    end
+    
     flush_json_object(@goods, Config::DB_JS)
   end
   
