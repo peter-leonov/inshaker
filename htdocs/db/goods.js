0dc3d9b7e9ac81b58ed2251d8facc399e2e15f9f
Peter Leonov <gojpeg@gmail.com>
2010-03-24 14:22:39 +0300

    first goods data

---
+++ htdocs/db/goods.js
@@ -0,0 +1,6 @@
+[
+{"name":"Шейкер","name_eng":"Shaker","path":"shaker","promos":4},
+{"name":"Бостонский шейкер","name_eng":"Boston Shaker","path":"boston-shaker","promos":3},
+{"name":"Сиропы","name_eng":"Cyrups","path":"cyrups","promos":3},
+{"name":"Мохито","name_eng":"Mojito","path":"mojito","promos":4}
+]
