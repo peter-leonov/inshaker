70342f208e468c1ba337ff5ff1ff94b00d6f9823
Maxim Dergilev <max@inshaker.ru>
2010-03-24 23:26:52 +0000

    content update

--- htdocs/db/goods.js
+++ htdocs/db/goods.js
@@ -1,7 +1,7 @@
 [
 {"name":"Шейкер","name_eng":"Shaker","path":"shaker","promos":4},
 {"name":"Бостонский шейкер","name_eng":"Boston Shaker","path":"boston-shaker","promos":3},
-{"name":"Сиропы","name_eng":"Cyrups","path":"cyrups","promos":3},
+{"name":"Сиропы","name_eng":"Syrups","path":"syrups","promos":3},
 {"name":"Мохито","name_eng":"Mojito","path":"mojito","promos":4},
 {"name":"Коктейльная книга Сити Спейс","name_eng":"City Space cocktail book","path":"city-space-cocktail-book","promos":4}
 ]
