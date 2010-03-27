128d00614ec01d44a052b61ce05b357bb380afde
Maxim Dergilev <max@inshaker.ru>
2010-03-27 14:31:52 +0000

    content update

--- htdocs/db/goods.js
+++ htdocs/db/goods.js
@@ -5,5 +5,6 @@
 {"name":"Мохито","name_eng":"Mojito","path":"mojito","promos":4},
 {"name":"Ангостура биттер","name_eng":"Angostura bitter","path":"angostura-bitter","promos":1},
 {"name":"Виски","name_eng":"Whiskey","path":"whiskey","promos":1},
+{"name":"Самбука","name_eng":"Sambuca","path":"sambuca","promos":3},
 {"name":"Коктейльная книга Сити Спейс","name_eng":"City Space cocktail book","path":"city-space-cocktail-book","promos":4}
 ]
