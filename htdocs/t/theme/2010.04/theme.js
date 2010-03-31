b7648b1d7b7b1e56d91403c660c5082b21728bf2
Peter Leonov <gojpeg@gmail.com>
2010-03-31 09:25:55 +0400

    branded link II

--- htdocs/t/theme/2010.04/theme.js
+++ htdocs/t/theme/2010.04/theme.js
@@ -1,4 +1,4 @@
 {
-	"spotlighted":{"href":"/cocktail/vodka_with_pears/"},
-	"branded-image":{"href":"/cocktail/vodka_with_pears/"}
+	"spotlighted":{"href":"/cocktail/pear_hit/"},
+	"branded-image":{"href":"/cocktail/pear_hit/"}
 }
