c3a85623659815f592c065f91762c3588841d6b6
Peter Leonov <gojpeg@gmail.com>
2010-04-04 13:43:21 +0400

    fix for plural

--- barman/lib/plural.rb
+++ barman/lib/plural.rb
@@ -2,7 +2,7 @@
 class Numeric
   # 1, 2, 5: банкир, банкира, банкиров
   def plural a, b, c
-    return b if self % 1
+    return b if self % 1 != 0
     
     v = self.abs % 100
     return c if 11 <= v && v <= 19
