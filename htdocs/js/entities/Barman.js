45213deb82a15a2128a233d2069f942e2ccf840f
Peter Leonov <gojpeg@gmail.com>
2010-04-14 14:15:00 +0400

    forget this nightmare

--- htdocs/js/entities/Barman.js
+++ htdocs/js/entities/Barman.js
@@ -20,53 +20,6 @@ Barman.prototype =
 		return '/barman/' + this.path + '/'
 	},
 	
-    switchable: true,
-    render: function (context)
-    {
-		Statistics.barmanPopupViewed(this)
-        var name = context.getElementsByTagName("h1")[0].firstChild
-        name.innerHTML = this.name
-        
-        var img  = context.getElementsByClassName("photo")[0]
-        img.style.backgroundImage  = "url(/i/barmen/" + this.name_eng.htmlName() + ".jpg)"
-        img.alt  = this.name
-           
-        var body = context.getElementsByClassName("desc")[0]
-        body.empty()
-        for(var i = 0; i < this.desc.length; i++)
-        {
-            var p = document.createElement("p")
-            p.innerHTML = this.desc[i]
-            body.appendChild(p)
-        }
-        
-        var parent  = context.getElementsByClassName('programica-rolling-images')[0]
-       
-		if(this.cocktails)
-        {
-            if(!parent.RollingImagesLite) 
-                new Programica.RollingImagesLite(parent, {animationType: 'easeInOutCubic'})
-            var surface = context.getElementsByClassName('surface')[0]
-            surface.empty()
-            parent.show()
-            for (var i = 0; i < this.cocktails.length; i++)
-		    {
-			    if (i % 6 === 0)
-			    {
-				    var point = document.createElement('ul')
-				    point.className = 'point'
-				    surface.appendChild(point)
-			    }
-				var cocktail = Cocktail.getByName(this.cocktails[i])
-				if (cocktail)
-					point.appendChild(cocktail.getPreviewNode())
-		    }
-		    parent.RollingImagesLite.sync()
-            parent.RollingImagesLite.goInit()
-        } 
-        else parent.hide()
-    },
-
     next: function () { return Barman.db[Barman.db.indexOf(this) + 1] },
     prev: function () { return Barman.db[Barman.db.indexOf(this) - 1] }
 }
