8c4e306a0173f726e4c00d6992527ead062fbec0
Peter Leonov <gojpeg@gmail.com>
2010-04-18 14:53:30 +0400

    restyle

--- barman/barman.rb
+++ barman/barman.rb
@@ -20,11 +20,11 @@ $stdout.sync = true
 Infinity = 1.0 / 0
 
 module Barman
-  ROOT_DIR = "/www/inshaker/"
-  BASE_DIR = ENV['BARMAN_BASE_DIR'] || (ROOT_DIR + "barman/base/")
-  LOCK_FILE = ".lock-barman"
+  ROOT_DIR      = "/www/inshaker/"
+  BASE_DIR      = ENV['BARMAN_BASE_DIR'] || (ROOT_DIR + "barman/base/")
+  LOCK_FILE     = ".lock-barman"
   
-  TEMPLATES_DIR = ROOT_DIR + "barman/templates/" 
+  TEMPLATES_DIR = ROOT_DIR + "barman/templates/"
   HTDOCS_DIR    = ROOT_DIR + "htdocs/"
   
   class Processor
