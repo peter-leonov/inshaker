29981c7f92630bddccc823dad9c351d6e4697ec2
Peter Leonov <gojpeg@gmail.com>
2010-04-20 13:13:25 +0400

    manual marks update

---
+++ htdocs/db/marks.js
@@ -0,0 +1,18 @@
+[
+{"name":"Absolut","path":"absolut"},
+{"name":"Angostura","path":"angostura"},
+{"name":"Antica","path":"antica"},
+{"name":"Bacardi","path":"bacardi"},
+{"name":"Barline","path":"barline"},
+{"name":"Black Bottle","path":"black-bottle"},
+{"name":"Bombay Sapphire","path":"bombay-sapphire"},
+{"name":"De Kuyper","path":"de-kuyper"},
+{"name":"Franciacorta","path":"franciacorta"},
+{"name":"Frapin","path":"frapin"},
+{"name":"Hakushika Ginjo Namachozo","path":"hakushika-ginjo-namachozo"},
+{"name":"Martini","path":"martini"},
+{"name":"Nonino","path":"nonino"},
+{"name":"Olmeca","path":"olmeca"},
+{"name":"Olmeca Altos","path":"olmeca-altos"},
+{"name":"Simple","path":"simple"}
+]
