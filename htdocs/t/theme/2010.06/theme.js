3a8a918b080106eaf2719051d14c8eb618a803bc
Peter Leonov <gojpeg@gmail.com>
2010-05-31 17:07:03 +0400

    2010.06 theme alpha

---
+++ htdocs/t/theme/2010.06/theme.js
@@ -0,0 +1,4 @@
+{
+	"spotlighted":{"href":"/cocktails.html#state=byIngredients&ingredients=%D0%97%D0%BE%D0%BB%D0%BE%D1%82%D0%B0%D1%8F%20%D1%82%D0%B5%D0%BA%D0%B8%D0%BB%D0%B0%20%D0%B8%D0%B7%20100%25%20%D0%B0%D0%B3%D0%B0%D0%B2%D1%8B"},
+	"branded-image":{"href":"/cocktails.html#state=byIngredients&ingredients=%D0%97%D0%BE%D0%BB%D0%BE%D1%82%D0%B0%D1%8F%20%D1%82%D0%B5%D0%BA%D0%B8%D0%BB%D0%B0%20%D0%B8%D0%B7%20100%25%20%D0%B0%D0%B3%D0%B0%D0%B2%D1%8B"}
+}
