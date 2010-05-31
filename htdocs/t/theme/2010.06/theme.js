f4a4d095680a22525e3c6fa7f1431ef3def7b910
Peter Leonov <gojpeg@gmail.com>
2010-05-31 17:54:38 +0400

    proper link II

--- htdocs/t/theme/2010.06/theme.js
+++ htdocs/t/theme/2010.06/theme.js
@@ -1,4 +1,4 @@
 {
-	"spotlighted":{"href":"/cocktail/tequila_sunrise/"},
-	"branded-image":{"href":"/cocktail/tequila_sunrise/"}
+	"spotlighted":{"href":"/cocktails.html#state=byIngredients&tag=%D0%BC%D0%B0%D1%80%D0%B3%D0%B0%D1%80%D0%B8%D1%82%D0%B0"},
+	"branded-image":{"href":"/cocktails.html#state=byIngredients&tag=%D0%BC%D0%B0%D1%80%D0%B3%D0%B0%D1%80%D0%B8%D1%82%D0%B0"}
 }
