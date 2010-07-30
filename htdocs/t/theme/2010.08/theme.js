79c792fb91c18aee3f047a6cbbdc663ca9f9237b
Peter Leonov <gojpeg@gmail.com>
2010-07-30 20:01:05 +0400

    link

--- htdocs/t/theme/2010.08/theme.js
+++ htdocs/t/theme/2010.08/theme.js
@@ -1,4 +1,4 @@
 {
-	"spotlighted":{"href":"/cocktail/martini_royale/"},
-	"branded-image":{"href":"/cocktail/martini_royale/"}
+	"spotlighted":{"href":"/cocktails.html#state=byIngredients&ingredients=%D0%9C%D0%B0%D0%BB%D0%B8%D0%B1%D1%83"},
+	"branded-image":{"href":"/cocktails.html#state=byIngredients&ingredients=%D0%9C%D0%B0%D0%BB%D0%B8%D0%B1%D1%83"}
 }
