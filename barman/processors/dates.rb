06f7f2ee83b1d301e9e002f7997387602d73f2e1
Peter Leonov <gojpeg@gmail.com>
2010-08-06 14:05:30 +0400

    iterate though commits

--- barman/processors/dates.rb
+++ barman/processors/dates.rb
@@ -3,18 +3,24 @@
 require "barman"
 
 class Processor < Barman::Processor
-
+  
+  module Config
+    ROOT = "/Users/peter/Desktop/dates/"
+  end
+  
   def job_name
     "угадывалку дат"
   end
   
   def job
     
-    commits = `git log --format=oneline`.scan(/^\w+/).reverse
+    commits = File.read("#{Config::ROOT}/commits.txt").scan(/^(\w{40}) (\d+)/).reverse
     
-    
-    
-    p commits
+    commits.each do |c|
+      hash = c[0]
+      date = Time.at(c[1].to_i)
+      say "#{date.strftime("%d.%m.%Y")} (#{hash})"
+    end
     
   end
 end
