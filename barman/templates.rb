3edb345e60a319a0c5b1ccf44ba327b842814e40
Peter Leonov <gojpeg@gmail.com>
2010-08-17 14:06:59 +0400

    tag -> group in html

--- barman/templates.rb
+++ barman/templates.rb
@@ -11,7 +11,7 @@ class CocktailTemplate
     @method      = hash["method"]
     @desc_start  = hash["desc_start"]
     @desc_end    = hash["desc_end"]
-    @tags        = hash["tags"]
+    @groups        = hash["groups"]
     @tools       = hash["tools"]
     @receipt     = hash["receipt"]
     @ingredients = hash["ingredients"]
@@ -26,8 +26,8 @@ class CocktailTemplate
     groups << ["/cocktails.html#method=#{@method}", @method]
     groups << ["/cocktails.html#strength=#{@strength}", @strength]
     
-    @tags.each do |tag|
-      groups << ["/cocktails.html#tag=#{tag}", tag]
+    @groups.each do |group|
+      groups << ["/cocktails.html#tag=#{group}", group]
     end
     groups
   end
