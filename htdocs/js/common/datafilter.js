fbe605433919e70ad3692e7cd4917619a44df6c3
Peter Leonov <gojpeg@gmail.com>
2010-08-17 14:30:15 +0400

    garbage collected

--- htdocs/js/common/datafilter.js
+++ htdocs/js/common/datafilter.js
@@ -144,57 +144,6 @@ self.DataFilter = {
 		return res;
 	},
 	
-	cocktailsByLetter: function (set, letter){
-		var res = [];	
-		var reg = new RegExp("^(" + letter.toUpperCase() + ")");
-		for(var i = 0; i < set.length; i++) {
-			if(set[i].name.match(reg)){
-				res.push(set[i]);
-			}
-		}
-		return res;
-	},
-	
-	cocktailsByTag: function (set, tag) {
-		var res = [];
-		for(var i = 0; i < set.length; i++){
-			if(set[i].tags.indexOf(tag) > -1){
-				res.push(set[i]);
-			}
-		}
-		return res;
-	},
-	
-	cocktailsByStrength: function(set, strength) {
-		var res = [];
-		for(var i = 0; i < set.length; i++){
-			if(set[i].strength == strength) {
-				res.push(set[i]);
-			}
-		}
-		return res;
-	},
-	
-	cocktailsByIngredients: function(set, ingredients) {
-		var res = [];
-		for(var i = 0; i < set.length; i++) {
-			var good = 0;
-			for(var j = 0; j < set[i].ingredients.length; j++) {
-				for(var k = 0; k < ingredients.length; k++){
-					if(set[i].ingredients[j][0] == ingredients[k]) good++;
-				}
-			}
-			if(good == ingredients.length) res.push(set[i]);
-		}
-		return res;
-	},
-	
-	nameSort: function(a, b){
-		if(a.name > b.name) return 1;
-		else if(a.name == b.name) return 0;
-		else return -1;
-	},
-	
 	/**
 	 * Нормализация объема относительно заданной единицы
 	 * @param normUnit - заданная единица (напр., "л")
