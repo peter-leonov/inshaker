813594b593956c46410c29deb26d08c997f704f1
Peter Leonov <gojpeg@gmail.com>
2010-08-23 18:07:32 +0400

    optional item key

--- barman/lib/array.rb
+++ barman/lib/array.rb
@@ -1,10 +1,16 @@
 # encoding: utf-8
 
 class Array
-  def hash_index key
+  def hash_index key=nil
     index = {}
-    each do |v|
-      index[v[key]] = v
+    if key
+      each do |v|
+        index[v[key]] = v
+      end
+    else
+      each do |v|
+        index[v] = v
+      end
     end
     return index
   end
