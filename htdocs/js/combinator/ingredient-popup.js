0ea855516c6aeabb36d005e833dae2dfb1b7327d
Peter Leonov <gojpeg@gmail.com>
2010-08-27 18:57:27 +0400

    plain copypaste from ingredients page

--- htdocs/js/combinator/ingredient-popup.js
+++ htdocs/js/combinator/ingredient-popup.js
@@ -2,37 +2,101 @@
 
 var myName = 'IngredientPopup'
 
-function Me ()
-{
-	this.nodes = {}
-	this.constructor = Me
-}
+function Me () {}
 
-Me.setup = function (nodes)
-{
-	var prototype = this.prototype
-	
-	prototype.commonNodes = nodes
-	
-	var cloner = prototype.cloner = new Cloner()
-	cloner.bind(nodes.popupMain, nodes.popupParts)
-}
+eval(NodesShortcut.include())
 
-// eval(NodesShortcut.include())
-
-Me.prototype =
+var methods =
 {
-	bind: function (nodes)
+	setup: function (nodes)
 	{
+		this.cache = {}
 		this.nodes = nodes
 		
+		var cloner = this.cloner = new Cloner()
+		cloner.bind(nodes.popupMain, nodes.popupParts)
+	},
+	
+	show: function (ingredient)
+	{
+		Statistics.ingredientPopupOpened(ingredient)
 		
+		var popup = this.cache[ingredient.name]
+		if (popup)
+		{
+			popup.show()
+			return
+		}
 		
-		return this
+		var clone = this.cloner.create()
+		this.nodes.root.appendChild(clone.root)
+		
+		var nodes = clone.nodes
+		var popup = new Popup()
+		this.cache[ingredient.name] = popup
+		popup.bind({root: clone.root, window: nodes.window, front: nodes.front})
+		popup.show()
+		
+		var brand = ingredient.brand
+		if (brand)
+		{
+			nodes.mark.appendChild(T(ingredient.brand))
+			nodes.ingredientWindow.addClassName('branded')
+			nodes.brand.appendChild(T(ingredient.mark))
+			nodes.brand.href = Ingredient.ingredientsLinkByMark(ingredient.mark)
+		}
+		
+		nodes.name.appendChild(T(ingredient.name))
+		
+		var len = ingredient.cocktails.length
+		if (len)
+			nodes.allLink.appendChild(T(' ' + len + ' ' + len.plural('коктейль', 'коктейля', 'коктейлей')))
+			
+		if (ingredient.decls)
+			nodes.allLink.appendChild(T(' ' + ingredient.decls.t))
+		nodes.allLink.href = ingredient.cocktailsLink()
+		
+		nodes.text.innerHTML = ingredient.about
+		
+		nodes.image.src = ingredient.getMainImageSrc()
+		
+		var me = this
+		setTimeout(function () { me.renderCocktails(nodes, ingredient.cocktails) }, 0)
+		require('Good', function () { me.renderWhereToBuy(nodes, ingredient) })
+	},
+	
+	renderWhereToBuy: function (popupNodes, ingredient)
+	{
+		var good = Good.getBySellName(ingredient.name)[0]
+		if (good)
+		{
+			popupNodes.ingredientWindow.addClassName('can-buy')
+			popupNodes.buy.appendChild(T(good.name))
+			popupNodes.buy.href = good.getHref()
+		}
+	},
+	
+	renderCocktails: function (popupNodes, cocktails)
+	{
+		cocktails = cocktails.slice().randomize()
+		
+		var cl = new CocktailList()
+		var nodes =
+		{
+			root: popupNodes.cocktails,
+			viewport: popupNodes.cocktailsViewport,
+			surface: popupNodes.cocktailsSurface,
+			prev: popupNodes.cocktailsPrev,
+			next: popupNodes.cocktailsNext
+		}
+		cl.bind(nodes)
+		cl.configure({pageLength: 5, pageVelocity: 38})
+		cl.setCocktails(cocktails)
 	}
 }
 
-// Me.mixIn(EventDriven)
+Object.extend(Me, methods)
+
 Me.className = myName
 self[myName] = Me
 
---
+++ htdocs/js/combinator/popup.js
@@ -0,0 +1,72 @@
+;(function(){
+
+var myName = 'Popup'
+
+function Me ()
+{
+	this.nodes = {}
+	this.listeners = {}
+	this.constructor = Me
+}
+
+Me.prototype =
+{
+	visible: false,
+	
+	bind: function (nodes)
+	{
+		this.nodes = nodes
+		
+		var me = this
+		this.listeners.click = function (e) { me.hide() }
+		this.listeners.key = function (e) { e.keyCode == 27 && me.hide() }
+		
+		nodes.window.addEventListener('click', function (e) { e.stopPropagation() }, false)
+		
+		return this
+	},
+	
+	hide: function ()
+	{
+		if (!this.visible)
+			return
+		
+		this.nodes.root.hide()
+		this.visible = false
+		
+		var me = this
+		setTimeout(function () { me.unbindListeners() }, 0)
+	},
+	
+	show: function ()
+	{
+		if (this.visible)
+			return
+		
+		var nodes = this.nodes
+		nodes.root.show()
+		nodes.front.style.top = (document.documentElement.scrollTop || document.body.scrollTop) + 'px'
+		this.visible = true
+		
+		var me = this
+		setTimeout(function () { me.bindListeners() }, 0)
+	},
+	
+	bindListeners: function ()
+	{
+		document.addEventListener('click', this.listeners.click, false)
+		document.addEventListener('keydown', this.listeners.key, false)
+	},
+	
+	unbindListeners: function ()
+	{
+		document.removeEventListener('click', this.listeners.click, false)
+		document.removeEventListener('keydown', this.listeners.key, false)
+	}
+}
+
+// Me.mixIn(EventDriven)
+Me.className = myName
+self[myName] = Me
+
+})();
