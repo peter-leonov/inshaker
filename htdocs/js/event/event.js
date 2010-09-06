ae0e209918e6c16da38424fe56ce732ec37b3415
Peter Leonov <gojpeg@gmail.com>
2010-09-06 17:32:25 +0400

    opera, msie

--- htdocs/js/event/event.js
+++ htdocs/js/event/event.js
@@ -38,6 +38,18 @@ function guessBrowser (ua)
 	if (m)
 		return classes('firefox', m[1], m[2], m[3])
 	
+	var m = /Opera\/\d+.+Version\/(\d+)\.(\d)(\d)/.exec(ua)
+	if (m)
+		return classes('opera', m[1], m[2], m[3])
+	
+	var m = /Opera\/(\d+)\.(\d)(\d)/.exec(ua)
+	if (m)
+		return classes('opera', m[1], m[2], m[3])
+	
+	var m = /MSIE (\d+)\./.exec(ua)
+	if (m)
+		return ['msie', 'msie-' + m[1]]
+	
 	var m = /Version\/(\d+)\.(\d+)\.(\d+) Safari\/\d+/.exec(ua)
 	if (m)
 		return classes('safari', m[1], m[2], m[3])
