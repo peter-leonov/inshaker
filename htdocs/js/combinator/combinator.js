71377133fca3796bb80c2625564fb3e8199bbf54
Peter Leonov <gojpeg@gmail.com>
2010-09-08 17:22:47 +0400

    set up popup first

--- htdocs/js/combinator/combinator.js
+++ htdocs/js/combinator/combinator.js
@@ -58,34 +58,6 @@ function onready ()
 {
 	var nodes =
 	{
-		output: $('output'),
-		cocktailList: $$('#output .result-block .ingrediented-cocktail-list')[0],
-		ingredientInput: $$('#search-box .ingredient')[0],
-		searchForm: $$('#search-box form')[0],
-		searchButton: $$('#search-box .search')[0],
-		ingredientComplete: $$('#search-box .autocomplete')[0],
-		totalCocktails: $$('#output .sort-line .cocktail-count')[0],
-		sortedWord: $$('#output .sort-line .sorted-word')[0],
-		suggestions: $$('#output .empty-block .suggestions')[0],
-		suggestionsList: $$('#output .empty-block .suggestions .list')[0],
-		ingredientsList: $$('#output .initial-block .ingredients-list')[0],
-		helpLine: $$('#output .help-line')[0],
-		hintSingle: $$('#output .help-line .hint.single')[0],
-		hintDouble: $$('#output .help-line .hint.double')[0],
-		
-		sortbySelect:
-		{
-			main: $('sortby-select'),
-			button: $$('#sortby-select .button')[0],
-			options: $$('#sortby-select .options')[0]
-		}
-	}
-	
-	var widget = new CombinatorPage()
-	widget.bind(nodes, {ingredient:Ingredient, cocktail:Cocktail}, {})
-	
-	var nodes =
-	{
 		root: document.body,
 		popupMain: $('ingredient-info-popup'),
 		popupParts:
@@ -111,6 +83,35 @@ function onready ()
 	IngredientPopup.setup(nodes)
 	
 	Ingredient.calculateEachIngredientUsage()
+	
+	
+	var nodes =
+	{
+		output: $('output'),
+		cocktailList: $$('#output .result-block .ingrediented-cocktail-list')[0],
+		ingredientInput: $$('#search-box .ingredient')[0],
+		searchForm: $$('#search-box form')[0],
+		searchButton: $$('#search-box .search')[0],
+		ingredientComplete: $$('#search-box .autocomplete')[0],
+		totalCocktails: $$('#output .sort-line .cocktail-count')[0],
+		sortedWord: $$('#output .sort-line .sorted-word')[0],
+		suggestions: $$('#output .empty-block .suggestions')[0],
+		suggestionsList: $$('#output .empty-block .suggestions .list')[0],
+		ingredientsList: $$('#output .initial-block .ingredients-list')[0],
+		helpLine: $$('#output .help-line')[0],
+		hintSingle: $$('#output .help-line .hint.single')[0],
+		hintDouble: $$('#output .help-line .hint.double')[0],
+		
+		sortbySelect:
+		{
+			main: $('sortby-select'),
+			button: $$('#sortby-select .button')[0],
+			options: $$('#sortby-select .options')[0]
+		}
+	}
+	
+	var widget = new CombinatorPage()
+	widget.bind(nodes, {ingredient:Ingredient, cocktail:Cocktail}, {})
 }
 
 $.onready(onready)
