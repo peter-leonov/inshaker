caabaeab00d27be56da547d5724626655e2648dc
Peter Leonov <gojpeg@gmail.com>
2010-09-13 19:38:46 +0400

    all cocktails tag

--- barman/processors/cocktails.rb
+++ barman/processors/cocktails.rb
@@ -352,6 +352,7 @@ class CocktailsProcessor < Barman::Processor
     end
     
     @cocktail["tags"] = about["Теги"] || []
+    @cocktail["tags"] << "Все коктейли"
     @cocktail["tags"].each do |tag|
       if @hidden_tags[tag]
         next
