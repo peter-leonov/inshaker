ff2bb5f98a234806d10cdd9e092edb12defff517
Peter Leonov <gojpeg@gmail.com>
2010-09-27 19:44:22 +0400

    merge saying and output

--- barman/lib/output.rb
+++ barman/lib/output.rb
@@ -1,7 +1,32 @@
 # encoding: utf-8
-require "lib/saying"
 
 module Output
+  module Saying
+    module Console
+      def say_error str
+        say "\x1B[31m#{str}\x1B[0m"
+      end
+      def say_warning str
+        say "\x1B[33m#{str}\x1B[0m"
+      end
+      def say_done str
+        say "\x1B[32m#{str}\x1B[0m"
+      end
+    end
+
+    module HTML
+      def say_error str
+        say %Q{<span style="color:#d40">#{str}</span>}
+      end
+      def say_warning str
+        say %Q{<span style="color:#e80">#{str}</span>}
+      end
+      def say_done str
+        say %Q{<span style="color:#082">#{str}</span>}
+      end
+    end
+  end
+  
   class Worker
     if ENV['REQUEST_METHOD']
       include Saying::HTML
