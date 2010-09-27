2302634c2f80f4ac2bce175d7f7ee8b90ae87eeb
Peter Leonov <gojpeg@gmail.com>
2010-09-27 19:54:34 +0400

    split cocktails config out of the processor

--- barman/processors/cocktails.rb
+++ barman/processors/cocktails.rb
@@ -1,30 +1,12 @@
 #!/opt/ruby1.9/bin/ruby -W0
 # encoding: utf-8
 require "inshaker"
+require "entities/cocktail"
 
 class CocktailsProcessor < Inshaker::Processor
 
   module Config
-    COCKTAILS_DIR = Inshaker::BASE_DIR + "Cocktails/"
-    HTDOCS_DIR    = Inshaker::HTDOCS_DIR
-    
-    HTDOCS_ROOT        = HTDOCS_DIR + "cocktail/"
-    DB_JS              = HTDOCS_DIR + "db/cocktails.js"
-    DB_JS_TAGS         = HTDOCS_DIR + "db/tags.js"
-    DB_JS_STRENGTHS    = HTDOCS_DIR + "db/strengths.js"
-    DB_JS_METHODS      = HTDOCS_DIR + "db/methods.js"
-    DB_JS_INGREDS      = HTDOCS_DIR + "db/ingredients.js"
-    DB_JS_INGRED_GROUPS= HTDOCS_DIR + "db/ingredients_groups.js"
-    
-    
-    NOSCRIPT_LINKS     = HTDOCS_ROOT + "links.html"
-    SITEMAP_LINKS      = HTDOCS_ROOT + "sitemap.txt"
-    
-    VIDEOS_DIR = HTDOCS_DIR + "v/"
-    
-    COCKTAIL_ERB  = Inshaker::TEMPLATES_DIR + "cocktail.rhtml"
-    RECOMENDATIONS_ERB  = Inshaker::TEMPLATES_DIR + "recomendations.rhtml"
-    RECOMENDATIONS_COUNT = 14
+    include Cocktail::Config
   end
   
   def initialize
