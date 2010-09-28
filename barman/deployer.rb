03fa15647763bda3dddbe163e5a5e22ff2e05432
Peter Leonov <gojpeg@gmail.com>
2010-09-28 20:08:17 +0400

    avoid such a deep conditional tree

--- barman/deployer.rb
+++ barman/deployer.rb
@@ -12,27 +12,32 @@ class Deployer < Inshaker::Processor
   
   def job
     Dir.chdir(Config::ROOT_DIR)
+    
     say "синхронизуюсь с сайтом…"
     unless system("git pull 2>&1")
       error "не удалось синхронизироваться с сайтом"
-    else
-      if `git status` =~ /nothing to commit/
-        warning "заливать нечего"
-      else
-        author = login_to_author(user_login)
-        say "сохраняю в гит…"
-        unless system(%Q{git add . && git commit -am "content update" --author="#{author.quote}" >>inshaker.log 2>&1})
-          error "не удалось сохранить обновления в гит"
-        else
-          say "заливаю на сайт…"
-          unless system("git push >>inshaker.log 2>&1")
-            error "не удалось залить обновления на сайт"
-          else
-            say "готово, проверяйте"
-          end
-        end
-      end
+      return 2
     end
+    
+    if `git status` =~ /nothing to commit/
+      warning "заливать нечего"
+      return 3
+    end
+    
+    author = login_to_author(user_login)
+    say "сохраняю в гит…"
+    unless system(%Q{git add . && git commit -am "content update" --author="#{author.quote}" >>inshaker.log 2>&1})
+      error "не удалось сохранить обновления в гит"
+      return 4
+    end
+    
+    say "заливаю на сайт…"
+    unless system("git push >>inshaker.log 2>&1")
+      error "не удалось залить обновления на сайт"
+      return 5
+    end
+    
+    say "готово, проверяйте"
   end
 end
 
