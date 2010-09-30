dc4238f3f7e863f296deef2d4e70eb8797ff4755
Peter Leonov <gojpeg@gmail.com>
2010-10-01 01:17:37 +0400

    link

--- htdocs/t/theme/2010.10/theme.js
+++ htdocs/t/theme/2010.10/theme.js
@@ -1,4 +1,4 @@
 {
-	"spotlighted":{"href":"/cocktails.html#ingredients=%D0%97%D0%BE%D0%BB%D0%BE%D1%82%D0%BE%D0%B9%20%D0%B2%D0%B5%D1%80%D0%BC%D1%83%D1%82&state=byIngredients"},
-	"branded-image":{"href":"/cocktails.html#ingredients=%D0%97%D0%BE%D0%BB%D0%BE%D1%82%D0%BE%D0%B9%20%D0%B2%D0%B5%D1%80%D0%BC%D1%83%D1%82&state=byIngredients"}
+	"spotlighted":{"href":"/cocktails.html#state=byIngredients&marks=Absolut"},
+	"branded-image":{"href":"/cocktails.html#state=byIngredients&marks=Absolut"}
 }
