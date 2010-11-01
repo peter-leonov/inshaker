934672b772be7a0e18eaa5f15d2a139908cf2240
Peter Leonov <gojpeg@gmail.com>
2010-11-01 20:13:19 +0300

    10 -> 11

--- htdocs/js/common/bars.js
+++ htdocs/js/common/bars.js
@@ -1,4 +1,4 @@
 <!--# include file="/js/common/maps.js" -->
 <!--# include file="/js/common/theme.js" -->
-Theme.initialize(<!--# include virtual="/t/theme/2010.10-bars/theme.js" -->)
+Theme.initialize(<!--# include virtual="/t/theme/2010.11-bars/theme.js" -->)
 $.onready(function () { Theme.bind() })
