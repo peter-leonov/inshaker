8f0fa3bffc9a9af121587d8307fdf7cc4d22d0d5
Peter Leonov <gojpeg@gmail.com>
2010-11-01 20:18:18 +0300

    new link for 2010.11

--- htdocs/t/theme/2010.11/theme.js
+++ htdocs/t/theme/2010.11/theme.js
@@ -1,4 +1,4 @@
 {
-	"spotlighted":{"href":"/cocktails.html#ingredients=%D0%97%D0%BE%D0%BB%D0%BE%D1%82%D0%B0%D1%8F%20%D1%82%D0%B5%D0%BA%D0%B8%D0%BB%D0%B0&state=byIngredients"},
-	"branded-image":{"href":"/cocktails.html#ingredients=%D0%97%D0%BE%D0%BB%D0%BE%D1%82%D0%B0%D1%8F%20%D1%82%D0%B5%D0%BA%D0%B8%D0%BB%D0%B0&state=byIngredients"}
+	"spotlighted":{"href":"/cocktails.html#name=%D0%B0%D1%84%D1%80%D0%BE%D0%B4%D0%B8%D0%B7%D0%B8%D0%B0%D0%BA&state=byName"},
+	"branded-image":{"href":"/cocktails.html#name=%D0%B0%D1%84%D1%80%D0%BE%D0%B4%D0%B8%D0%B7%D0%B8%D0%B0%D0%BA&state=byName"}
 }
