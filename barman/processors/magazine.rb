1ec95b5044a0391d530aa6c32c70cc812f009cf7
Peter Leonov <gojpeg@gmail.com>
2010-11-18 13:49:59 +0300

    links are gone

--- barman/processors/magazine.rb
+++ barman/processors/magazine.rb
@@ -67,33 +67,11 @@ class MagazineProcessor < Inshaker::Processor
     indent do
     update_cocktails(@db["cocktails"] = about['Коктейли'])
     end # indent
-    
-    say "обновляю линки"
-    indent do
-    update_entities @db["links"] = about['Inshaker рекомендует'], "links", "png", "линка"
-    end # indent
   end
   
   def update_cocktails entities
     entities.each do |entity|
       say entity
-    end
-  end
-  
-  def update_entities entities, name, ext, kind,
-    basedir = Config::BASE_DIR + name
-    htdir = Config::HT_ROOT + name
-    FileUtils.mkdir_p htdir
-    
-    i = 1
-    entities.each do |entity|
-      name, href = entity[0], entity[1]
-      say name
-      indent do
-      fname = "#{i}.#{ext}"
-      copy_image "#{basedir}/#{fname}", "#{htdir}/#{fname}", kind, 150
-      end # indent
-      i += 1
     end
   end
   
