db09dddb8cd9e835e8d5f4974faa957b3643ea7a
Peter Leonov <gojpeg@gmail.com>
2010-11-21 21:26:29 +0300

    wrap data in instances

--- htdocs/js/entities/Video.js
+++ htdocs/js/entities/Video.js
@@ -1,9 +1,19 @@
 ;(function(){
 
-var Me =
+function Me (data)
+{
+	this.name = data.name
+	this.movie = data.movie
+	this.cocktails = data.cocktails
+}
+
+Me.prototype =
 {
 	initialize: function (db)
 	{
+		for (var i = 0, il = db.length; i < il; i++)
+			db[i] = new this(db[i])
+		
 		this.db = db
 	},
 	
