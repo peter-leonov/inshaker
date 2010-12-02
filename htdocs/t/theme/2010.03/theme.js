b900599cac434333ba3063e314988e0f26717d5f
Peter Leonov <gojpeg@gmail.com>
2010-12-03 02:20:48 +0300

    collect old cocktails and bars themes

--- htdocs/t/theme/2010.03/theme.js
+++
@@ -1,4 +0,0 @@
-{
-	"spotlighted":{"href":"/cocktails.html#state=byIngredients&marks=Angostura"},
-	"branded-image":{"href":"/cocktails.html#state=byIngredients&marks=Angostura"}
-}
--- htdocs/t/theme/2010.05/theme.js
+++
@@ -1,4 +0,0 @@
-{
-	"spotlighted":{"href":"/cocktails.html#ingredients=%D0%92%D0%B8%D1%81%D0%BA%D0%B8&state=byIngredients"},
-	"branded-image":{"href":"/cocktails.html#ingredients=%D0%92%D0%B8%D1%81%D0%BA%D0%B8&state=byIngredients"}
-}
