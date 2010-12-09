ad777f66cbe8da1fc88c896ffce87f8b186356bc
Peter Leonov <gojpeg@gmail.com>
2010-12-10 02:14:54 +0300

    use GoogleAnalytics wrapper to track events at the point account is already set up

--- htdocs/js/common/oops.js
+++ htdocs/js/common/oops.js
@@ -1,3 +1,6 @@
+<!--# include virtual="/lib-0.3/modules/google-analytics.js" -->
+GoogleAnalytics.setAccount('UA-1635720-11')
+
 <!--# include virtual="/lib-0.3/modules/tracker-ga.js" -->
 <!--# include virtual="/lib-0.3/modules/oops.js" -->
 Oops.maybeEnable()
