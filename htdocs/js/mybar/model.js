1ce99e45eb6bb74802b1f9be5ac9b32331f19ac3
SerjoPepper <SerjoPepper@gmail.com>
2010-12-09 19:12:46 +0300

    fetch ingredients for cocktails

--- htdocs/js/mybar/model.js
+++ htdocs/js/mybar/model.js
@@ -6,8 +6,28 @@ var myProto =
 {
 	bind : function ()
 	{
-		this.view.renderCocktails(Cocktail.getAll().slice(50,55))
-		this.view.renderIngredients(Ingredient.getAll().slice(50,55))
+		this.cocktails = Cocktail.getAll().slice(50,55)
+		this.ingredients = this.fetchIngredints(this.cocktails)
+		this.view.renderCocktails(this.cocktails)
+		this.view.renderIngredients(this.ingredients)
+	},
+	
+	fetchIngredints : function(cocktails)
+	{
+		var ingr = {}
+		for (var i = 0, il = cocktails.length; i < il; i++)
+		{
+			var cocktailIngr = cocktails[i].ingredients.slice().map(function(a){ return a[0] })
+			for( var j = 0; j < cocktailIngr.length; j++ )
+				ingr[cocktailIngr[j]] = true
+		}
+		
+		var ingredients = []
+		for( i in ingr )
+			ingredients.push(Ingredient.getByName(i))
+			
+		return ingredients.sort(function(a,b){ return a.name > b.name ? 1 : -1 })
+		
 	}
 }
 
