7731c3e0ac60657cacdf036bcedf7836ef4a75dc
Peter Leonov <gojpeg@gmail.com>
2010-12-10 02:17:27 +0300

    just call a method ;)

--- htdocs/js/common/analytics.js
+++ htdocs/js/common/analytics.js
@@ -1,21 +1,5 @@
 ;(function(){
 
-var account = 'UA-1635720-11'
-
-if (!window._gaq)
-	window._gaq = []
-
-window._gaq.push(['_setAccount', account])
-window._gaq.push(['_trackPageview'])
-
-// async loading of async ga.js :)
-function load ()
-{
-	// untouched inclusion snippet
-	var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-	ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-	(document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(ga);
-}
-$.onready(function () { setTimeout(function () { load() }, 250) })
+$.onready(function () { setTimeout(function () { GoogleAnalytics.trackPageview() }, 250) })
 
 })();
