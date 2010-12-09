dd425d40fd317b6e5213d8f7e58b9d752fc8ac36
Peter Leonov <gojpeg@gmail.com>
2010-12-09 17:13:37 +0300

    move Oops out from the giant common scripts

--- htdocs/js/common/common.js
+++ htdocs/js/common/common.js
@@ -1,7 +1,3 @@
-<!--# include virtual="/lib-0.3/modules/tracker-ga.js" -->
-<!--# include virtual="/lib-0.3/modules/oops.js" -->
-Oops.maybeEnable()
-
 <!--# include file="/lib/Programica/Init.js" -->
 <!--# include file="/lib/Programica/Fixes.js" -->
 <!--# include file="/lib/Programica/OnReady.js" -->
