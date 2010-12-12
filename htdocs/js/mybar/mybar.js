b87a83607e9a59cbd332ca4b84fe3a28f75b2cdf
SerjoPepper <SerjoPepper@gmail.com>
2010-12-12 04:14:58 +0300

    add old lib

--- htdocs/js/mybar/mybar.js
+++ htdocs/js/mybar/mybar.js
@@ -21,7 +21,8 @@ Object.extend(Me.prototype, myProto)
 
 
 <!--# include virtual="/lib-0.3/modules/json.js" -->
-<!--# include virtual="/lib-0.3/modules/user-agent.js" -->
+//Sorry for old lib. It's needed for Storage (hard code in it)
+<!--# include virtual="/lib/Programica/UA.js" -->
 <!--# include virtual="/js/common/storage.js" -->
 <!--# include virtual="model.js" -->
 <!--# include virtual="view.js" -->
