f6771fa139e176a8d991ebf031431525ef6dce1e
Peter Leonov <gojpeg@gmail.com>
2011-01-31 22:03:39 +0300

    link

--- htdocs/t/theme/2011.02/theme.js
+++ htdocs/t/theme/2011.02/theme.js
@@ -1,4 +1,4 @@
 {
-	"spotlighted":{"href":"/combinator.html#q=ABSOLUT%20shooters"},
-	"branded-image":{"href":"/combinator.html#q=ABSOLUT%20shooters"}
+	"spotlighted":{"href":"/combinator.html#q=%D0%90%D0%BD%D0%B3%D0%BE%D1%81%D1%82%D1%83%D1%80%D0%B0"},
+	"branded-image":{"href":"/combinator.html#q=%D0%90%D0%BD%D0%B3%D0%BE%D1%81%D1%82%D1%83%D1%80%D0%B0"}
 }
