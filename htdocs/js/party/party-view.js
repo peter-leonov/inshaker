d08edfe1bd4a0a11c395faadbb4fc9a2a7537ca0
Peter Leonov <gojpeg@gmail.com>
2011-02-15 21:15:25 +0300

    switch “editing state”

--- htdocs/js/party/party-view.js
+++ htdocs/js/party/party-view.js
@@ -14,6 +14,8 @@ Me.prototype =
 		this.nodes = nodes
 		
 		nodes.count.addEventListener('keydown', function (e) { if (e.keyCode == 13) { this.blur(); e.preventDefault() } }, false)
+		nodes.count.addEventListener('focus', function (e) { this.addClassName('focused') }, false)
+		nodes.count.addEventListener('blur', function (e) { this.removeClassName('focused') }, false)
 		
 		return this
 	}
