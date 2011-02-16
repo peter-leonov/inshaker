8b02e793879db8ad92529f7094c9015490e82adf
Peter Leonov <gojpeg@gmail.com>
2011-02-17 00:20:40 +0300

    clean the value rather than prevent changing

--- htdocs/js/party/party-view.js
+++ htdocs/js/party/party-view.js
@@ -13,40 +13,29 @@ Me.prototype =
 	{
 		this.nodes = nodes
 		
-		function keydown (e)
-		{
-			if (e.keyCode == 13)
-			{
-				this.blur()
-				e.preventDefault()
-			}
-		}
-		
-		var allow = {8: 1, 33: 1, 34: 1, 37: 1, 38: 1, 39: 1, 40: 1, 46: 1, 48: 1, 49: 1, 50: 1, 51: 1, 52: 1, 53: 1, 54: 1, 55: 1, 56: 1, 57: 1}
-		
 		function keypress (e)
 		{
-			log(e.keyCode)
-			
 			if (e.keyCode == 13)
 			{
 				this.blur()
 				e.preventDefault()
 			}
-			
-			if (e.ctrlKey || e.metaKey)
-				return
-			
-			if (allow[e.keyCode])
-				return
-			
-			e.preventDefault()
 		}
 		
-		// nodes.count.addEventListener('keydown', keydown, false)
+		function blur (e)
+		{
+			this.removeClassName('focused')
+			
+			var v = (this.firstChild && this.firstChild.nodeValue) || ''
+			
+			v = parseInt(v.replace(/\D|^0+/g, ''))
+			
+			this.innerHTML = isNaN(v) ? 0 : v
+		}
+		
 		nodes.count.addEventListener('keypress', keypress, false)
 		nodes.count.addEventListener('focus', function (e) { this.addClassName('focused') }, false)
-		nodes.count.addEventListener('blur', function (e) { this.removeClassName('focused'); if (!this.firstChild || !this.firstChild.nodeValue) this.innerHTML = 0 }, false)
+		nodes.count.addEventListener('blur', blur, false)
 		
 		return this
 	}
