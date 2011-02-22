5dd3a21803efc79d3e5fd254ea1e08b155c76700
SerjoPepper <SerjoPepper@gmail.com>
2011-02-22 15:38:45 +0300

    select nodes and add classes for popups

--- htdocs/js/mybar/mybar.js
+++ htdocs/js/mybar/mybar.js
@@ -59,18 +59,24 @@ Object.extend(Me.prototype, myProto)
 <!--# include virtual="/lib-0.3/modules/motion-types.js" -->
 <!--# include virtual="/lib-0.3/modules/animation.js" -->
 <!--# include virtual="/lib-0.3/modules/user-agent.js" -->
+<!--# include virtual="/lib-0.3/modules/regexp-escape.js" -->
+
+<!--# include virtual="/lib-0.3/modules/child-indexed-path.js"-->
+<!--# include virtual="/lib-0.3/modules/cloner.js"-->
 
 //Sorry for old lib. It's needed for Storage (hard code in it)
 <!--# include virtual="/lib/Programica/UA.js" -->
 <!--# include virtual="/js/combinator/throttler.js" -->
-<!--# include virtual="ingrediented-cocktail-list.js" -->
+<!--# include virtual="/js/combinator/popup.js" -->
+<!--# include virtual="/js/combinator/ingredient-popup.js" -->
+
 <!--# include virtual="/js/common/storage.js" -->
 
 <!--# include virtual="/js/common/autocompleter-3.js" -->
 <!--# include virtual="/js/common/plain-input-autocompleter.js" -->
 
 <!--# include virtual="/js/cocktails/ingredients-searcher.js" -->
-<!--# include virtual="/lib-0.3/modules/regexp-escape.js" -->
+<!--# include virtual="ingrediented-cocktail-list.js" -->
 
 <!--# include virtual="model.js" -->
 <!--# include virtual="view.js" -->
@@ -82,6 +88,37 @@ function onready ()
 {
 	UserAgent.setupDocumentElementClassNames()
 	document.documentElement.removeClassName('loading')
+	
+	var nodes =
+	{
+		root: document.body,
+		popupMain: $('ingredient-info-popup'),
+		popupParts:
+		{
+			window: $$('#ingredient-info-popup .popup-window')[0],
+			front: $$('#ingredient-info-popup .popup-front')[0],
+			ingredientWindow: $$('#ingredient-info-popup .popup-window .ingredient-window')[0],
+			image: $$('#ingredient-info-popup .description .image')[0],
+			mark: $$('#ingredient-info-popup .description .about .mark')[0],
+			brand: $$('#ingredient-info-popup .description .about .brand .link')[0],
+			buy: $$('#ingredient-info-popup .description .about .where-to-buy .link')[0],
+			name: $$('#ingredient-info-popup .description .about .name')[0],
+			text: $$('#ingredient-info-popup .description .about .text')[0],
+			allCocktails: $$('#ingredient-info-popup .description .about .all-cocktails')[0],
+			allCocktailsLink: $$('#ingredient-info-popup .description .about .all-cocktails .link')[0],
+			combinations: $$('#ingredient-info-popup .description .about .combinations')[0],
+			combinationsList: $$('#ingredient-info-popup .description .about .combinations .list')[0],
+			cocktails: $$('#ingredient-info-popup .cocktail-list')[0],
+			cocktailsViewport: $$('#ingredient-info-popup .cocktail-list .viewport')[0],
+			cocktailsSurface: $$('#ingredient-info-popup .cocktail-list .surface')[0],
+			cocktailsPrev: $$('#ingredient-info-popup .cocktail-list .prev')[0],
+			cocktailsNext: $$('#ingredient-info-popup .cocktail-list .next')[0]
+		}
+	}
+	
+	IngredientPopup.setup(nodes)
+	
+	Ingredient.calculateEachIngredientUsage()
 	
 	var nodes = {
 		
@@ -127,7 +164,8 @@ function onready ()
 			mustHave : $$('#output .bottom-output .must-have')[0]
 		},
 		
-		menuLink : $$('#output .cocktails-block .bar-link')[0]
+		menuLink : $$('#output .cocktails-block .bar-link')[0],
+		output : $('output')
 	}
 
 	var widget = new MyBar()
