48b3687a9e187f2ca027f0d82ba50294fa968622
Peter Leonov <gojpeg@gmail.com>
2011-02-28 15:59:13 +0300

    link

--- htdocs/t/theme/2011.03/theme.js
+++ htdocs/t/theme/2011.03/theme.js
@@ -1,4 +1,4 @@
 {
-	"spotlighted":{"href":"/cocktails.html#state=byIngredients&tag=%D0%BC%D0%B0%D1%80%D0%B3%D0%B0%D1%80%D0%B8%D1%82%D0%B0"},
-	"branded-image":{"href":"/cocktails.html#state=byIngredients&tag=%D0%BC%D0%B0%D1%80%D0%B3%D0%B0%D1%80%D0%B8%D1%82%D0%B0"}
+	"spotlighted":{"href":"/cocktail/gold_margarita/"},
+	"branded-image":{"href":"/cocktail/gold_margarita/"}
 }
