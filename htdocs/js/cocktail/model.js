5a76713e7815b9fd324547a4949ab07352ccd2e7
Peter Leonov <gojpeg@gmail.com>
2011-03-04 19:17:12 +0300

    handy array proto extension

--- htdocs/js/cocktail/model.js
+++ htdocs/js/cocktail/model.js
@@ -1,3 +1,11 @@
+Array.prototype.hashIndex = function ()
+{
+	var hash = {}
+	for (var i = 0, il = this.length; i < il; i++)
+		hash[this[i]] = true
+	return hash
+}
+
 var Model = {
 	cocktail: null,
 	ingredients: [],
