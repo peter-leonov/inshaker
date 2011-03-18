98b4b9307a647b6fb8e4e645c7302749cb2fc441
Peter Leonov <gojpeg@gmail.com>
2011-03-18 17:22:21 +0300

    exchange once more

--- htdocs/js/common/rolling-images.js
+++ htdocs/js/common/rolling-images.js
@@ -122,17 +122,17 @@ RollingImagesLite.prototype =
 		if (this.aPrev)
 		{
 			if (this.current > 0)
-				this.aPrev.addClassName('disabled')
-			else
 				this.aPrev.removeClassName('disabled')
+			else
+				this.aPrev.addClassName('disabled')
 		}
 		
 		if (this.aNext)
 		{
 			if (this.current < this.points.length - 1)
-				this.aPrev.addClassName('disabled')
+				this.aNext.removeClassName('disabled')
 			else
-				this.aPrev.removeClassName('disabled')
+				this.aNext.addClassName('disabled')
 		}
 	},
 	
