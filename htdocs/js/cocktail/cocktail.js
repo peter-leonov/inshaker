ce3fe029b1231fc3e422d3bb579e0bfcac86738f
Peter Leonov <gojpeg@gmail.com>
2011-03-18 16:37:53 +0300

    rolling images, again

--- htdocs/js/cocktail/cocktail.js
+++ htdocs/js/cocktail/cocktail.js
@@ -1,13 +1,17 @@
 $.onready(function(){
 	var aniOpts = {animationType: 'easeInOutCubic'};
-	new Programica.RollingImagesLite($('recommendations'), aniOpts);
-	new Programica.RollingImagesLite($('related'), aniOpts);
-	new Programica.RollingImagesLite($('ingredients'), aniOpts);
+	new RollingImagesLite($('recommendations'), aniOpts);
+	new RollingImagesLite($('related'), aniOpts);
+	new RollingImagesLite($('ingredients'), aniOpts);
 	Controller.init();
 	Calculator.init();
 	Theme.bind()
 })
 
+<!--# include file="/lib-0.3/modules/motion.js" -->
+<!--# include file="/lib-0.3/modules/motion-types.js" -->
+<!--# include file="/lib-0.3/modules/animation.js" -->
+<!--# include file="/js/common/rolling-images.js" -->
 <!--# include file="/js/calculator/calculator.js" -->
 <!--# include file="/js/cocktail/model.js" -->
 <!--# include file="/js/cocktail/controller.js" -->
