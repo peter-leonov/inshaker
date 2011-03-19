1422f0997081b48f2a8755b37566ea2b32097c80
Peter Leonov <gojpeg@gmail.com>
2011-03-19 03:08:55 +0300

    include all really persistent backends

--- htdocs/js/common/cocktails.js
+++ htdocs/js/common/cocktails.js
@@ -19,6 +19,11 @@ Array.prototype.uniq = function(){
 
 <!--# include file="/lib-0.3/modules/json.js" -->
 <!--# include file="/lib-0.3/modules/client-storage.js" -->
+<!--# include file="/lib-0.3/modules/client-storage/local-storage.js" -->
+<!--# include file="/lib-0.3/modules/client-storage/global-storage.js" -->
+<!--# include file="/lib-0.3/modules/client-storage/user-data.js" -->
+<!--# include file="/lib-0.3/modules/client-storage/flash-9.js" -->
+
 var clientStorage = ClientStorage.guess()
 if (!clientStorage)
 	throw new Error('no client storge was found')
