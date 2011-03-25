a5d3feb0ce6828ecab281f1e3b0bc8f332d64e92
SerjoPepper <SerjoPepper@gmail.com>
2011-03-26 02:34:12 +0300

    add event listeners, correct some render

--- htdocs/js/purchase-plan/view.js
+++ htdocs/js/purchase-plan/view.js
@@ -14,16 +14,7 @@ var myProto =
 		
 		var me = this
 		
-		nodes.purchasePlan.wrapper.addEventListener('click', function(e){ me.maybeEditPlanItem(e) }, false)
-	},
-	
-	maybeEditPlanItem : function(e)
-	{
-		var target = e.target
-		if(target.editableItem)
-		{
-			this.controller.editPlanItem(target.editableItem, target.exclude)
-		}
+		nodes.purchasePlan.wrapper.addEventListener('click', function(e){ me.handleTableClicks(e) }, false)
 	},
 	
 	renderBarName : function(barName)
@@ -74,9 +65,15 @@ var myProto =
 			//item volume
 			{
 				var volumeTd = Nc('td', 'item-volume')
-				var volume = Nct('span', 'volume-value', volumes[name].volume)
-				volume.setAttribute('contenteditable', true)
-				volume.volumeIngredient = ingredient
+				var volume = Nct('span', 'volume-value', exclude ? 0 : volumes[name].volume)
+				//if(!exclude)
+				{
+					volume.setAttribute('contenteditable', true)
+					volume.ingredient = ingredient
+					volume.row = tr
+					volume.editNode = edit
+					this.appendEventsToVolumeField(volume)
+				}
 				var unit = Nct('span', 'volume-unit', ingredient.unit)
 				volumeTd.appendChild(volume)
 				volumeTd.appendChild(unit)
@@ -86,7 +83,8 @@ var myProto =
 			
 			//item price
 			{
-				var priceTd = Nct('td', 'item-price', volumes[name].price)
+				var priceTd = Nct('td', 'item-price', exclude ? 0 : volumes[name].price)
+				volume.priceNode = priceTd
 				tr.appendChild(priceTd)
 			}
 			
@@ -94,8 +92,10 @@ var myProto =
 			{
 				var noticeTd = Nc('td', 'item-notice')
 				var notice = Nct('div', 'notice-text', notes[name] || '')
-				notice.noticeIngredient = ingredient
 				notice.setAttribute('contenteditable', true)
+				notice.ingredient = ingredient
+				this.appendEventsToNoticeField(notice)
+
 				noticeTd.appendChild(notice)
 				tr.appendChild(noticeTd)
 			}
@@ -104,21 +104,6 @@ var myProto =
 		}
 		
 		//total
-		{
-			var tr = Nc('tr', 'total-price')
-			var td = N('td')
-			td.setAttribute('colspan', 2)
-			
-			var totalLabel = Nct('td', 'total-label', 'Итого')
-			var total = Nct('td', 'total', totalPrice)
-			
-			tr.appendChild(td)
-			tr.appendChild(totalLabel)
-			tr.appendChild(total)
-			tr.appendChild(N('td'))
-			
-			df.appendChild(tr)
-		}
 		
 		var nodes = this.nodes.purchasePlan
 		
@@ -126,7 +111,114 @@ var myProto =
 		nodes.body.empty()
 		nodes.body.appendChild(df)
 		
+		this.renderTotalPrice(totalPrice)
+		
 	},
+	
+	renderTotalPrice : function(totalPrice)
+	{
+		var nodes = this.nodes.purchasePlan
+		nodes.totalPrice.empty()
+		nodes.totalPrice.appendChild(T(totalPrice))
+	},
+	
+	renderNewPrice : function(price)
+	{
+		var tr = this.currentEditingField.row
+		var editNode = this.currentEditingField.editNode
+		var priceNode = this.currentEditingField.priceNode
+		if(!price)
+		{
+			tr.removeClassName('included')
+			tr.addClassName('excluded')
+			editNode.exclude = true
+			editNode.empty()
+			editNode.appendChild(T('+'))		
+		}
+		else
+		{
+			tr.removeClassName('excluded')
+			tr.addClassName('included')		
+			editNode.exclude = false
+			editNode.empty()
+			editNode.appendChild(T('×'))
+		}
+		
+		priceNode.empty()
+		priceNode.appendChild(T(price))
+	},
+	
+	currentEditingField : null,
+	
+	appendEventsToVolumeField : function(node)
+	{
+		var me = this
+		var availableCharCodes = { 48:1, 49:1, 50:1, 51:1, 52:1, 53:1, 54:1, 55:1, 56:1, 57:1, 46:1, 44:1 }
+		var availableKeyCodes = { 8:1, 37:1, 39:1 }
+		var keypress = function(e)
+		{
+			var charCode = e.charCode
+			var keyCode = e.keyCode
+			
+			
+			if(keyCode == 13)
+			{
+				e.target.blur()
+				e.preventDefault()
+				return
+			}
+			
+			if(!availableCharCodes[charCode] && !availableKeyCodes[keyCode])
+			{
+				e.preventDefault()
+				return
+			}
+			
+			var target = e.target
+			me.currentEditingField = target
+			var ingredient = target.ingredient
+			setTimeout(function(){ me.controller.setVolume(ingredient, target.innerHTML) }, 0)
+		}
+		
+		node.addEventListener('keypress', function(e){ keypress(e) }, false)
+	},
+	
+	appendEventsToNoticeField : function(node)
+	{
+		var me = this
+		var suppressKeys = {9:1, 16:1, 17:1, 27:1, 33:1, 34:1, 35:1, 36:1, 37:1, 38:1, 39:1, 18:1, 91:1}
+		var keypress = function(e)
+		{
+			var charCode = e.charCode
+			
+			if(charCode == 13)
+			{
+				e.target.blur()
+				e.preventDefault()
+				return
+			}
+			if(suppressKeys[charCode])
+			{
+				e.preventDefault()
+				return
+			}
+			
+			var target = e.target
+			var ingredient = target.ingredient
+			setTimeout(function(){ me.controller.setNotice(ingredient, target.innerHTML) }, 0)
+		}
+		
+		node.addEventListener('keypress', function(e){ keypress(e) }, false)	
+	},
+	
+	handleTableClicks : function(e)
+	{
+		var target = e.target
+		if(target.editableItem)
+		{
+			this.controller.editPlanItem(target.editableItem, target.exclude)
+		}
+	},	
 	
 	renderIfEmpty : function()
 	{
