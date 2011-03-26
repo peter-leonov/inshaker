62cfd9d64b6b836e1d3204693403301594fecff5
SerjoPepper <SerjoPepper@gmail.com>
2011-03-26 16:05:47 +0300

    fix!!!

--- htdocs/js/common/find-cheapest-price.js
+++ htdocs/js/common/find-cheapest-price.js
@@ -2,25 +2,19 @@ var findCheapestPrice = function(ingredient, v)
 {		
 	var ingredientVolumes = ingredient.volumes
 	ingredientVolumes.map(function(a){ a.nominalPrice = a[1]/a[0] })
-	ingredientVolumes.sort(function(a, b){ return a.nominalPrice - b.nominalPrice })
+	ingredientVolumes.sort(function(a, b){ return b.nominalPrice - a.nominalPrice })
+	
+	var last = ingredientVolumes.length - 1
 	
 	var minNominal = ingredientVolumes[0].nominalPrice
 	
-	console.log(ingredientVolumes)
-	
-	var bottleVolume = ingredientVolumes[0][0]
+	var bottleVolume = ingredientVolumes[last][0]
 	var bottles = Math.ceil(v / bottleVolume)
-	var pricePerBottle = ingredientVolumes[0][1]
+	var pricePerBottle = ingredientVolumes[last][1]
 	var volume = bottles * bottleVolume
 	var totalPrice = bottles * pricePerBottle
 	var minPrice = totalPrice
 	var returnObj = { bottles : [], price : minPrice, addingBottles : { amount : bottles, vol : bottleVolume, pricePerBottle : pricePerBottle } }
-	
-	var f = true
-	
-	console.log(returnObj)
-	
-	ingredientVolumes.sort(function(a, b){ return b.nominalPrice - a.nominalPrice })
 	
 	appendBottles(volume, minPrice, 0, [], bottles)
 	
@@ -33,22 +27,15 @@ var findCheapestPrice = function(ingredient, v)
 			var tbottles = addingBottles
 			var temporyPrice = currentPrice
 			
-			console.log('i', i)
-			
-			while(tbottles > 0 && v - vol <= volumeObj[0] && i != il - 1 )
+			while(tbottles > 0 && v - vol <= volumeObj[0] && i != last )
 			{
 				vol -= bottleVolume
 				tbottles--
 				temporyPrice -= pricePerBottle
 				
 			}
-			f = false
-			console.log('tbottles', tbottles)
-			console.log('-----------------------------')
 			
 			var tprice = temporyPrice + volumeObj[1]
-			
-			console.log(bottles)
 			
 			if(tprice < minPrice)
 			{
@@ -72,9 +59,6 @@ var findCheapestPrice = function(ingredient, v)
 	return returnObj
 }
 
-var ingredient = { volumes : [[0.5, 37.90], [1.0, 52.90], [2.0, 56.90]]
- }
+var ingredient = { volumes : [[0.5, 37.90], [1.0, 52.90], [2.0, 56.90]] }
 
 var res = findCheapestPrice(ingredient, 2.5)
-
-// console.log(res)
