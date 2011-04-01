0b4e1785e72c9243df9e3f3438ddc6e7b04c617f
Peter Leonov <gojpeg@gmail.com>
2011-04-01 15:25:47 +0400

    setup cocktails

--- htdocs/js/party/party-model.js
+++ htdocs/js/party/party-model.js
@@ -2,14 +2,28 @@
 
 function Me ()
 {
-	
+	this.cocktails = []
 }
 
 Me.prototype =
 {
 	setPartyName: function (name)
 	{
-		log(Party.getByName(name))
+		this.party = Party.getByName(name)
+		
+		this.setupCocktails(this.party.cocktails)
+	},
+	
+	setupCocktails: function (source)
+	{
+		var cocktails = this.cocktails
+		for (var i = 0, il = source.length; i < il; i++)
+		{
+			var s = source[i]
+			cocktails.push({cocktail: Cocktail.getByName(s.name), factor: s.factor})
+		}
+		
+		this.view.renderCocktails(cocktails)
 	}
 }
 
