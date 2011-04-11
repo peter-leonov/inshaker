369039d35a74555d59074b33a1dac0468d56cdd3
SerjoPepper <SerjoPepper@gmail.com>
2011-04-11 17:32:31 +0400

    create mvc files for foreign, get all methods from mybar mvc

---
+++ htdocs/js/foreign/model.js
@@ -0,0 +1,142 @@
+;(function(){
+
+Array.toHash = function(arr)
+{
+	var hash = {}
+	for (var i = 0, il = arr.length; i < il; i++)
+		hash[arr[i]] = true
+	return hash
+}
+
+Object.toArray = function(obj)
+{
+	var arr = []
+	for( var k in obj )
+		if(obj[k])
+			arr.push(k)
+	return arr
+}
+
+var Me = Foreign.Model
+
+var myProto =
+{
+	bind : function ()
+	{
+		var me = this
+		BarStorage.initBar(function(bar){ me.setBar(bar) })
+	},
+
+	setBar : function(bar)
+	{
+		this.barName = bar.barName
+		this.ingredients = this.getIngredients(bar.ingredients)
+		this.cocktails = this.computeCocktails(this.ingredients)
+		
+		var me = this
+		this.ingredients.sort(function(a,b){ return me.sortByUsage(a,b) })
+		
+		this.parent.setMainState()	
+	},
+
+	setMainState : function()
+	{
+		this.view.renderBarName(this.barName)
+		this.view.renderIngredients(this.ingredients)
+		this.view.renderCocktails(this.cocktails)
+	},
+	
+	sortByUsage : function(a, b)
+	{
+		if(a.group != b.group)
+			return Ingredient.sortByGroups(a.name, b.name)
+
+		var u = this.ingredients.usage
+		
+		var r = (u[b.name] || 0) - (u[a.name] || 0)
+
+		return r != 0 ? r : a.name.localeCompare(b.name)
+	},
+	
+	getIngredients : function(ingredientNames)
+	{
+		var ingredients = []
+		for (var i = 0, il = ingredientNames.length; i < il; i++)
+		{
+			ingredients.push(Ingredient.getByName(ingredientNames[i]))
+		}
+		
+		ingredients.inBar = Array.toHash(ingredientNames)
+		return ingredients.sort(function(a, b){ return Ingredient.sortByGroups(a.name, b.name) })
+	},
+	
+	computeCocktails : function(ingredients)
+	{
+		if(Object.isEmpty(ingredients.inBar)) return []
+		var needCocktails = Cocktail.getByIngredientNames(Object.toArray(ingredients.inBar), {count : 1}),
+			cocktails = []
+			
+		ingredients.usage = {}
+
+		ck:
+		for ( var i = 0, il = needCocktails.length; i < il; i++ )
+		{
+			var cocktail = needCocktails[i]
+			var ing = cocktail.ingredients
+			for (var j = 0, jl = ing.length; j < jl; j++)
+			{
+				if(!ingredients.inBar[ing[j][0]]) 
+					continue ck
+			}
+
+			for (var k = 0, kl = ing.length; k < kl; k++) 
+			{
+				var ingr = ing[k][0]
+				if(!ingredients.usage[ingr])
+					ingredients.usage[ingr] = 1
+				else
+					ingredients.usage[ingr]++
+			}
+			
+			cocktails.push(cocktail)
+		}
+		
+		return cocktails.sort(this.sortCocktails)
+	},
+	
+	sortCocktails : function(a, b)
+	{	
+		var t = a.ingredients.length - b.ingredients.length
+		if(t)
+			return t
+		
+		var ai = a.ingredients,
+			bi = b.ingredients,
+			al = ai.length,
+			bl = bi.length,
+			lc = 0
+		
+		for (var i = 0, il = al < bl ? al : bl; i < il; i++) 
+		{
+			var aa = Ingredient.getByName(ai[i][0]),
+				bb = Ingredient.getByName(bi[i][0])
+			
+			if(aa.group != bb.group)
+				return Ingredient.sortByGroups(aa.name, bb.name)
+
+			lc = aa.name.localeCompare(bb.name)
+			if(lc)
+				return lc
+		}
+		return lc
+	},
+	
+	selectIngredient : function(ingredient)
+	{
+		this.view.showIngredient(ingredient)
+	}
+}
+
+Object.extend(Me.prototype, myProto)
+
+})();
---
+++ htdocs/js/foreign/view.js
@@ -0,0 +1,103 @@
+;(function(){
+
+var Me = Foreign.View
+eval(NodesShortcut.include())
+var myProto =
+{
+	bind : function (nodes)
+	{
+		this.nodes = nodes
+		var me = this
+		nodes.output.addEventListener('click', function(e){ me.maybeIngredientClicked(e.target) }, false)
+	},
+	
+	renderBarName : function(barName)
+	{
+		barName = barName || 'Безымянный бар'
+		
+		this.nodes.barName.empty()
+		this.nodes.barName.appendChild(T(barName))
+		this.nodes.title.empty()
+		this.nodes.title.appendChild(T(barName + ' — Inshaker'))
+	},
+	
+	renderIngredients : function(ingredients)
+	{
+		var ingr = this.nodes.ingredients
+		
+		if(ingredients.length == 0)
+		{
+			ingr.list.empty()
+			ingr.empty.show()
+			return
+		}
+		
+		ingr.empty.hide()
+		
+		var ul = N('ul')
+		for(var i = 0, l = ingredients.length; i < l; i++)
+		{
+			var li = Nc('li', 'ingredient')
+			li.appendChild(ingredients[i].getPreviewNode())
+			ul.appendChild(li)
+		}
+		
+		ingr.list.empty()
+		ingr.list.appendChild(ul)		
+	},
+	
+	renderCocktails : function(cocktails)
+	{
+		var cl = cocktails.length
+		
+		var c = this.nodes.cocktails
+		c.amount.empty()
+		c.amount.appendChild(T(cl + ' ' + cl.plural('коктейля', 'коктейлей', 'коктейлей')))
+		
+		if(cocktails.length == 0)
+		{
+			c.empty.show()
+			c.wrapper.hide()
+			return
+		}
+		
+		c.empty.hide()
+		
+		var ul = Nc('ul', 'photos-list')
+		for (var i = 0, il = cocktails.length; i < il; i++) 
+		{
+			var cNode = cocktails[i].getPreviewNode()
+			ul.appendChild(cNode)
+		}
+		c.wrapper.empty()
+		c.wrapper.appendChild(ul)
+		
+		c.wrapper.show()		
+	},
+	
+	maybeIngredientClicked : function(target)
+	{
+		if(!target.parentNode)
+			return
+		
+		var ingredient = target.parentNode['data-ingredient']
+		if(ingredient)
+			this.controller.ingredientSelected(ingredient)
+	},
+	
+	showIngredient: function (ingredient)
+	{
+		if (ingredient)
+		{
+			var popup = IngredientPopup.show(ingredient)
+			var controller = this.controller
+			popup.onhide = function () { controller.ingredientSelected(null) }
+		}
+		else
+			IngredientPopup.hide()
+	}
+}
+
+Object.extend(Me.prototype, myProto)
+
+})();
