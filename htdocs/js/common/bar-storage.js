9101b699330e90bd299f24403dc3291db5f4e937
SerjoPepper <SerjoPepper@gmail.com>
2011-04-27 23:55:23 +0400

    another fix for chrome

--- htdocs/js/common/bar-storage.js
+++ htdocs/js/common/bar-storage.js
@@ -53,7 +53,7 @@ Me =
 			}
 			
 			parsingJson = JSON.parse(json)
-			me.newbie = parsingJson.ingredients ? true : false
+			me.newbie = !!(parsingJson.ingredients || false)
 			Object.extend(me.bar, parsingJson)
 			
 			if(callback)
