44677bf58804118f6ca0300a3243685b19a7053e
SerjoPepper <SerjoPepper@gmail.com>
2011-06-01 13:40:03 +0400

    change paths

--- htdocs/js/mybar/mybar.js
+++ htdocs/js/mybar/mybar.js
@@ -37,8 +37,8 @@ Object.extend(Me.prototype, myProto)
 <!--# include virtual="/lib-0.3/modules/cloner.js"-->
 
 <!--# include virtual="/js/combinator/throttler.js" -->
-<!--# include virtual="/js/combinator/popup.js" -->
-<!--# include virtual="/js/combinator/ingredient-popup.js" -->
+<!--# include virtual="/js/common/popup.js" -->
+<!--# include virtual="/js/common/ingredient-popup.js" -->
 
 <!--# include virtual="/js/common/bar-storage.js" -->
 <!--# include virtual="/js/common/mybar-name.js" -->
@@ -61,35 +61,7 @@ function onready ()
 	UserAgent.setupDocumentElementClassNames()
 	document.documentElement.removeClassName('loading')
 	
-	var nodes =
-	{
-		root: document.body,
-		popupMain: $('ingredient-info-popup'),
-		popupParts:
-		{
-			window: $$('#ingredient-info-popup .popup-window')[0],
-			front: $$('#ingredient-info-popup .popup-front')[0],
-			ingredientWindow: $$('#ingredient-info-popup .popup-window .ingredient-window')[0],
-			image: $$('#ingredient-info-popup .description .image')[0],
-			inBar : $$('#ingredient-info-popup .description .about .in-bar')[0],
-			mark: $$('#ingredient-info-popup .description .about .mark')[0],
-			brand: $$('#ingredient-info-popup .description .about .brand .link')[0],
-			buy: $$('#ingredient-info-popup .description .about .where-to-buy .link')[0],
-			name: $$('#ingredient-info-popup .description .about .name')[0],
-			text: $$('#ingredient-info-popup .description .about .text')[0],
-			allCocktails: $$('#ingredient-info-popup .description .about .all-cocktails')[0],
-			allCocktailsLink: $$('#ingredient-info-popup .description .about .all-cocktails .link')[0],
-			combinations: $$('#ingredient-info-popup .description .about .combinations')[0],
-			combinationsList: $$('#ingredient-info-popup .description .about .combinations .list')[0],
-			cocktails: $$('#ingredient-info-popup .cocktail-list')[0],
-			cocktailsViewport: $$('#ingredient-info-popup .cocktail-list .viewport')[0],
-			cocktailsSurface: $$('#ingredient-info-popup .cocktail-list .surface')[0],
-			cocktailsPrev: $$('#ingredient-info-popup .cocktail-list .prev')[0],
-			cocktailsNext: $$('#ingredient-info-popup .cocktail-list .next')[0]
-		}
-	}
-	
-	IngredientPopup.setup(nodes)
+	IngredientPopup.bootstrap()
 	
 	Ingredient.calculateEachIngredientUsage()
 	
