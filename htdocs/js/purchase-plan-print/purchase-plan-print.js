2b2ee2d5b9e842f3635b2cbe3481be7824422917
SerjoPepper <SerjoPepper@gmail.com>
2011-06-02 18:00:48 +0400

    rename file

---
+++ htdocs/js/purchase-plan-print/purchase-plan-print.js
@@ -0,0 +1,55 @@
+;(function(){
+
+var myName = 'PurchasePlan',
+	Me = self[myName] = MVC.create(myName)
+
+var myProto =
+{
+	bind : function (nodes)
+	{
+		this.view.bind(nodes)
+		this.model.bind()
+		this.controller.bind()
+	},
+	
+	setMainState : function()
+	{
+		this.model.setMainState()
+		return this
+	}
+}
+
+Object.extend(Me.prototype, myProto)
+
+})();
+
+<!--# include virtual="/lib-0.3/modules/json.js" -->
+<!--# include virtual="/lib-0.3/modules/user-agent.js" -->
+
+<!--# include virtual="/js/common/bar-storage.js" -->
+<!--# include virtual="/js/common/purchase-plan-table.js" -->
+
+<!--# include virtual="model.js" -->
+<!--# include virtual="view.js" -->
+<!--# include virtual="controller.js" -->
+
+;(function(){
+
+function onready ()
+{
+	UserAgent.setupDocumentElementClassNames()
+	document.documentElement.removeClassName('loading')
+	
+	var nodes = {
+		barName : $$('#common-main-wrapper .header .bar-name')[0],
+		totalPrice : $$('#common-main-wrapper .purchase-plan-box .total-price-wrapper .total-price')[0],
+		wrapper : $$('#common-main-wrapper .purchase-plan-box .purchase-table-wrapper')[0]
+	}
+	
+	var widget = new PurchasePlan()
+	widget.bind(nodes)
+}
+
+$.onready(onready)
+
+})();
