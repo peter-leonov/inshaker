00a004de1821b05be7ff4b941c1ab2539909769a
SerjoPepper <SerjoPepper@gmail.com>
2011-06-02 18:15:32 +0400

    remove useless handlers

--- htdocs/js/cocktail-cart/controller.js
+++ htdocs/js/cocktail-cart/controller.js
@@ -7,16 +7,6 @@ var myProto =
 	bind : function()
 	{
 		
-	},
-	
-	addCocktailToBarMenu : function(cocktailName)
-	{
-		this.model.addCocktailToBarMenu(cocktailName)
-	},
-	
-	removeCocktailFromBarMenu : function(cocktailName)
-	{
-		this.model.removeCocktailFromBarMenu(cocktailName)
 	}
 }
 
