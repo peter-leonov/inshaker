a57aafa05c7041da11f6c68de831478d80d96254
SerjoPepper <SerjoPepper@gmail.com>
2011-06-17 20:03:30 +0400

    move widget to common.js

--- htdocs/js/mybar/mybar.js
+++ htdocs/js/mybar/mybar.js
@@ -40,9 +40,6 @@ Object.extend(Me.prototype, myProto)
 <!--# include virtual="/js/common/popup.js" -->
 <!--# include virtual="/js/common/ingredient-popup.js" -->
 
-<!--# include virtual="/js/common/bar-storage.js" -->
-<!--# include virtual="/js/common/mybar-button.js" -->
-
 <!--# include virtual="/js/common/autocompleter-3.js" -->
 <!--# include virtual="/js/common/plain-input-autocompleter.js" -->
 
