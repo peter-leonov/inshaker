e905c37e0f4cba15f8fba9d26c174d88b2cc02f2
SerjoPepper <SerjoPepper@gmail.com>
2011-06-20 23:00:38 +0400

    add ajax libs, put bar-storage after client-storage classes

--- htdocs/js/common/cocktails.js
+++ htdocs/js/common/cocktails.js
@@ -23,11 +23,14 @@ Array.prototype.uniq = function(){
 <!--# include virtual="/lib-0.3/modules/client-storage/global-storage.js" -->
 <!--# include virtual="/lib-0.3/modules/client-storage/user-data.js" -->
 <!--# include virtual="/lib-0.3/modules/client-storage/flash-9.js" -->
-<!--# include virtual="/js/common/bar-storage.js" -->
 
 var clientStorage = ClientStorage.guess()
 if (!clientStorage)
 	throw new Error('no client storge was found')
+
+<!--# include virtual="/lib-0.3/modules/url-encode.js"-->
+<!--# include virtual="/lib-0.3/modules/request.js"-->
+<!--# include virtual="/js/common/bar-storage.js" -->
 
 <!--# include virtual="/lib-0.3/modules/global-timer.js" -->
 <!--# include virtual="/lib-0.3/modules/motion.js" -->
