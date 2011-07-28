519165ff86383829990b8de1020cf79f249ec451
Peter Leonov <gojpeg@gmail.com>
2011-07-28 16:53:26 +0400

    lock what? inshaker? no… barman!

--- barman/config.rb
+++ barman/config.rb
@@ -4,7 +4,7 @@ module Inshaker
   DOMAIN        = "www.inshaker.ru"
   ROOT_DIR      = "/www/inshaker/"
   BASE_DIR      = ENV['INSHAKER_BASE_DIR'] || (ROOT_DIR + "barman/base/")
-  LOCK_FILE     = ".lock-inshaker"
+  LOCK_FILE     = ".lock-barman"
   
   TEMPLATES_DIR = ROOT_DIR + "barman/templates/"
   HTDOCS_DIR    = ROOT_DIR + "htdocs/"
