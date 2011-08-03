9752087002c8c345460a6cc14e95198aaac03747
Peter Leonov <gojpeg@gmail.com>
2011-08-03 20:08:40 +0400

    load under mouse

--- htdocs/js/blog/blog.js
+++ htdocs/js/blog/blog.js
@@ -1,7 +1,21 @@
 
 function onready (e)
 {
-	alert(1)
+	function click (e)
+	{
+		var target = e.target
+		if (target.lazyLoaded)
+			return
+		
+		var src = target.getAttribute('lazy-src')
+		if (src)
+		{
+			target.src = src
+			target.lazyLoaded = true
+		}
+	}
+	
+	document.addEventListener('mousemove', click, false)
 }
 
 document.addEventListener('ready', onready, false)
