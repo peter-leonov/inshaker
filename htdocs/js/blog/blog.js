9d9eac273e15a3e8b9317c695e96aada60c1b6a5
Peter Leonov <gojpeg@gmail.com>
2011-08-03 20:25:44 +0400

    lazy load images (finally!)

--- htdocs/js/blog/blog.js
+++ htdocs/js/blog/blog.js
@@ -1,21 +1,66 @@
+;(function(){
+
+function Me ()
+{
+	var nodes = []
+}
+
+Me.prototype =
+{
+	bind: function (nodes)
+	{
+		this.setupVisibilityFrame(nodes.lazyImages)
+	},
+	
+	setupVisibilityFrame: function (images)
+	{
+		var boxes = Boxer.nodesToBoxes(images)
+		
+		var frame = this.frame = new VisibilityFrame()
+		frame.setFrame(4000, 5000) // hardcoded for now
+		frame.setStep(1000, 1000)
+		frame.moveTo(0, -2500)
+		
+		var me = this
+		frame.onmove = function (show, hide)
+		{
+			for (var i = 0; i < show.length; i++)
+			{
+				var box = show[i]
+				if (!box.loaded)
+				{
+					var node = box.node
+					
+					node.src = node.getAttribute('lazy-src')
+					node.removeClassName('lazy')
+					
+					box.loaded = true
+				}
+			}
+		}
+		
+		frame.setBoxes(boxes)
+		
+		var t = new Throttler(function (y) { frame.moveTo(0, y - 2500) }, 100, 500)
+		window.addEventListener('scroll', function () { t.call(window.pageYOffset) }, false)
+	}
+}
+
+Me.className = 'BlogMainPage'
+self[Me.className] = Me
+
+})();
+
 
 function onready (e)
 {
-	function click (e)
+	var nodes =
 	{
-		var target = e.target
-		if (target.lazyLoaded)
-			return
-		
-		var src = target.getAttribute('lazy-src')
-		if (src)
-		{
-			target.src = src
-			target.lazyLoaded = true
-		}
+		lazyImages: $$('#posts-loop .post .body .image.lazy')
 	}
 	
-	document.addEventListener('mousemove', click, false)
+	var widget = new BlogMainPage()
+	widget.bind(nodes)
 }
 
 document.addEventListener('ready', onready, false)
