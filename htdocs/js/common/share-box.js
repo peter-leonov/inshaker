e0b6796e5cf50108bb0e65c85ea30963e927d63f
Peter Leonov <gojpeg@gmail.com>
2011-08-18 15:11:15 +0400

    handy search parent nodes function

--- htdocs/js/common/share-box.js
+++ htdocs/js/common/share-box.js
@@ -37,6 +37,22 @@ Me.prototype =
 		window.open(href, '', 'left=' + l + ',top=' + t + ',width=' + w + ',height=' + h + ',personalbar=0,toolbar=0,scrollbars=1,resizable=1')
 	},
 	
+	searchParentNodes: function (root, node, f)
+	{
+		for (;;)
+		{
+			if (!node)
+				break
+			
+			var v = f(node)
+			if (v !== undefined)
+				return v
+			
+			if (node == root)
+				break
+			
+			node = node.parentNode
+		}
 	}
 }
 
