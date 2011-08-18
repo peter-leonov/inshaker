94da516ed8f29c40926b6caab9ea375b9831a7c6
Peter Leonov <gojpeg@gmail.com>
2011-08-18 15:10:58 +0400

    readable windowOpen form mybar

--- htdocs/js/common/share-box.js
+++ htdocs/js/common/share-box.js
@@ -17,6 +17,26 @@ Me.prototype =
 		for (var i = 0, il = buttons.length; i < il; i++)
 			buttons[i].href = buttons[i].href.replace('${link}', url)
 		
+	
+	openWindow: function (href)
+	{
+		var w = 550, h = 312, l = 0, t = 0,
+			sh = window.screen.height,
+			sw = window.screen.width
+		
+		if (sw < w)
+			w = sw
+		else
+			l = Math.round((sw - w) / 2)
+		
+		if (sh < h)
+			h = sh
+		else
+			t = Math.round((sh - h) / 2)
+		
+		window.open(href, '', 'left=' + l + ',top=' + t + ',width=' + w + ',height=' + h + ',personalbar=0,toolbar=0,scrollbars=1,resizable=1')
+	},
+	
 	}
 }
 
