fa0651d3012e893febdb7b5730402a2c90fc8e33
Peter Leonov <gojpeg@gmail.com>
2011-10-18 17:45:28 +0400

    event() not track()

--- htdocs/js/common/statistics.js
+++ htdocs/js/common/statistics.js
@@ -6,67 +6,67 @@ var Me =
 {
 	magazinePromoViewed: function (promo)
 	{
-		this.track('magazine-promo-viewed', promo && promo.name)
+		this.event('magazine-promo-viewed', promo && promo.name)
 	},
 	
 	cocktailsFilterSelected: function (name)
 	{
-		this.track('cocktails-filter-selected', name)
+		this.event('cocktails-filter-selected', name)
 	},
 	
 	cocktailViewRecipe: function (cocktail)
 	{
-		this.track('cocktail-view-recipe', cocktail && cocktail.name)
+		this.event('cocktail-view-recipe', cocktail && cocktail.name)
 	},
 	
 	cocktailViewLegend: function (cocktail)
 	{
-		this.track('cocktail-view-legend', cocktail && cocktail.name)
+		this.event('cocktail-view-legend', cocktail && cocktail.name)
 	},
 	
 	cocktailAddedToCalculator: function (cocktail)
 	{
-		this.track('cocktail-added-to-calculator', cocktail && cocktail.name)
+		this.event('cocktail-added-to-calculator', cocktail && cocktail.name)
 	},
 	
 	toolPopupOpened: function (tool)
 	{
-		this.track('tool-popup', tool && tool.name)
+		this.event('tool-popup', tool && tool.name)
 	},
 	
 	ingredientPopupOpened: function (ingredient)
 	{
-		this.track('ingredient-popup', ingredient && ingredient.name)
+		this.event('ingredient-popup', ingredient && ingredient.name)
 	},
 	
 	ingredientTypedIn: function (ingredient)
 	{
-		this.track('ingredient-typed-in', ingredient && ingredient.name)
+		this.event('ingredient-typed-in', ingredient && ingredient.name)
 	},
 	
 	ingredientSelected: function (ingredient)
 	{
-		this.track('ingredient-selected', ingredient && ingredient.name)
+		this.event('ingredient-selected', ingredient && ingredient.name)
 	},
 	
 	combinatorQueryRaw: function (query)
 	{
-		this.track('combinator-query-raw', query)
+		this.event('combinator-query-raw', query)
 	},
 	
 	combinatorQueryViewed: function (query)
 	{
-		this.track('combinator-query', query)
+		this.event('combinator-query', query)
 	},
 	
 	blogTagSelected: function (tag)
 	{
-		this.track('blog-tag-selected', tag)
+		this.event('blog-tag-selected', tag)
 	},
 	
-	track: function (action, label, value)
+	event: function (action, label, value)
 	{
-		setTimeout(function () { Tracker.track('UserAction', action, label, value) }, 500)
+		setTimeout(function () { Tracker.event('UserAction', action, label, value) }, 500)
 	}
 }
 
