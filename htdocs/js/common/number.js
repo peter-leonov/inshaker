46db3374251c97e279324c210e90632300d37e19
Peter Leonov <gojpeg@gmail.com>
2011-10-19 03:50:22 +0400

    cache math functions

--- htdocs/js/common/number.js
+++ htdocs/js/common/number.js
@@ -1,14 +1,19 @@
 ;(function(){
 
+var M = Math,
+	pow = M.pow,
+	round = M.round,
+	ceil = M.ceil
+
 Number.prototype.round = function (factor)
 {
 	if (factor)
 	{
-		factor = Math.pow(10, factor)
-		return Math.round(this * factor) / factor
+		factor = pow(10, factor)
+		return round(this * factor) / factor
 	}
 	
-	return Math.round(this)
+	return round(this)
 }
 
 })();
