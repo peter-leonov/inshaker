251866a792b1bf786aeaa45e63d6e6c45c8b09b9
Peter Leonov <gojpeg@gmail.com>
2011-11-11 20:07:50 +0400

    re-style

--- htdocs/js/entities/Tool.js
+++ htdocs/js/entities/Tool.js
@@ -2,33 +2,40 @@
 
 function Me (data)
 {
-	for (var k in data) this[k] = data[k];
+	for (var k in data)
+		this[k] = data[k]
 }
 
-Me.prototype = {
-	constructor: Me,
-	
-	imgSrc: function(){
-		return "/i/merchandise/tools/" + this.name.trans() + ".png";
+Me.prototype =
+{
+	imgSrc: function ()
+	{
+		return '/i/merchandise/tools/' + this.name.trans() + '.png'
 	}
 }
 
 Me.staticMethods =
 {
-	tools: [],
+	db: null,
 	
-	initialize: function (array)
+	initialize: function (db)
 	{
-		for(var i = 0; i < array.length; i++){
-			var tool = new Tool(array[i]);
-			this.tools[i] = tool;
+		this.db = db
+		
+		for (var i = 0; i < db.length; i++)
+		{
+			db[i] = new Tool(db[i])
 		}
 	},
 	
 	getByName: function (name)
 	{
-		for(var i = 0; i < this.tools.length; i++){
-			if(this.tools[i].name == name) return this.tools[i];
+		var db = this.db
+		for (var i = 0; i < this.db.length; i++)
+		{
+			var tool = db[i]
+			if (tool.name == name)
+				return tool
 		}
 	}
 }
