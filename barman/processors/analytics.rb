cc35aba227d3a7f3a0e07628b12bb6c751eb5f26
Peter Leonov <gojpeg@gmail.com>
2011-11-15 01:11:33 +0400

    can log in

--- barman/processors/analytics.rb
+++ barman/processors/analytics.rb
@@ -9,16 +9,42 @@ require "config"
 class Analytics
   
   module Config
-    
+    PROFILE_ID     = "9038802"
+    BASE_DIR       = Inshaker::BASE_DIR + "Blog/"
+    AUTH_URI       = "https://www.google.com/accounts/ClientLogin"
+    LOGIN          = ENV["ANALYTICS_EMAIL"]
+    PASSWORD       = ENV["ANALYTICS_PASSWORD"]
   end
   
   def job
     
-    update
+    if login
+      update
+    end
     
     unless errors?
       flush_json
     end
+  end
+  
+  def login
+    # based on http://gdatatips.blogspot.com/2008/08/perform-clientlogin-using-curl.html
+    io = IO.popen(["curl", Config::AUTH_URI, "-s", "-d", "accountType=GOOGLE", "-d" "Email=#{Config::LOGIN}", "-d", "Passwd=#{Config::PASSWORD}", "-d", "service=analytics", "-d", "source=inshaker"])
+    r = io.read
+    io.close
+    
+    
+    token = /^Auth=(\S+)/.match(r)
+    unless token
+      error "не удалось залогиниться"
+      return false
+    end
+    
+    @token = token[1]
+    
+    p @token
+    
+    return true
   end
   
   def update
