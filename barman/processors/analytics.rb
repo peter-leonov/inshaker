de1923c7770645538f94356367800ee2e16b6d63
Peter Leonov <gojpeg@gmail.com>
2011-11-15 00:44:22 +0400

    a real processor

--- barman/processors/analytics.rb
+++ barman/processors/analytics.rb
@@ -2,7 +2,47 @@
 # encoding: utf-8
 
 require "lib/json"
+require "lib/output"
+
 require "config"
 
+class Analytics
+  
+  module Config
+    
+  end
+  
+  def job
+    
+    update
+    
+    unless errors?
+      flush_json
+    end
+  end
+  
+  def update
+    
+  end
+  
+  def flush_json
+    
+  end
+  
+  def run
+    begin
+      job
+      summary
+    rescue => e
+      error "Паника: #{e.to_s.force_encoding('UTF-8')}"
+      raise e
+    end
+    
+    return errors_count
+  end
+  
+end
 
-puts "hi!"
+
+$stdout.sync = true
+exit Analytics.new.run
