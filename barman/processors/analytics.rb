b9e468f45369e7a2186f50a42f849bbba8c35c23
Peter Leonov <gojpeg@gmail.com>
2011-11-17 03:43:40 +0400

    typo

--- barman/processors/analytics.rb
+++ barman/processors/analytics.rb
@@ -139,7 +139,7 @@ class Analytics
   @@substitute =
   {
     "---_on_the_beach" => "bitch_on_the_beach.html",
-    "safe_---_on_the_beach.html" => "safe_sex_on_the_beach",
+    "safe_---_on_the_beach" => "safe_sex_on_the_beach",
     "---_on_the_beach_light" => "sex_on_the_beach_light"
   }
   
