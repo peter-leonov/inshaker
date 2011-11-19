0a44a59602b7db0c3b84c56b2ed0b51f4eb0bda1
Peter Leonov <gojpeg@gmail.com>
2011-11-20 00:38:13 +0400

    merge periods

--- htdocs/js/entities/Stats.js
+++ htdocs/js/entities/Stats.js
@@ -13,6 +13,49 @@ Me =
 		var periods = Object.keys(this.db)
 		periods.sort()
 		return periods
+	},
+	
+	getPeriod: function (name)
+	{
+		return this.db[name]
+	},
+	
+	mergePeriods: function (periods)
+	{
+		var res = {}
+		
+		for (var i = 0, il = periods.length; i < il; i++)
+			this.addHashes(res, this.getPeriod(periods[i]))
+		
+		return res
+	},
+	
+	addHashes: function (a, b)
+	{
+		var c = {}
+		
+		// just copy
+		for (var k in a)
+		{
+			var src = a[k]
+			c[k] = [src[0], src[1]]
+		}
+		
+		for (var k in b)
+		{
+			var src = b[k]
+			
+			var dst = c[k]
+			if (dst)
+			{
+				dst[0] += src[0]
+				dst[1] += src[1]
+			}
+			else
+				c[k] = [src[0], src[1]]
+		}
+		
+		return c
 	}
 }
 
