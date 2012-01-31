f3f9f8ac6538732aa1c65c3f68f7abb4d35cf97b
Peter Leonov <gojpeg@gmail.com>
2012-01-31 04:24:13 +0400

    update branding applying

--- htdocs/t/branding/2012.02-bars/branding.js
+++ htdocs/t/branding/2012.02-bars/branding.js
@@ -1,4 +1,39 @@
+;(function(){
+
+var Me =
 {
-	"spotlighted":{"href":"/cocktail/mojito/"},
-	"branded-image":{"href":"/cocktail/mojito/"}
+	initialize: function (db)
+	{
+		this.db = db
+	},
+	
+	bind: function ()
+	{
+		var db = this.db
+		
+		for (var k in db)
+		{
+			var item = db[k]
+			if (!item.href)
+				continue
+			
+			var node = $(k)
+			if (node)
+			{
+				node.href = item.href
+			}
+		}
+	}
 }
+
+var link = '/cocktail/martini_royale/'
+
+Me.initialize
+({
+	'spotlighted':{'href':link},
+	'branded-image':{'href':link}
+})
+
+Me.bind()
+
+})();
