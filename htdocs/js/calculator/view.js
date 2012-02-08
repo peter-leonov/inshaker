80a7d3f29e68f6dba379d770e323efba3d9bbcd2
Peter Leonov <gojpeg@gmail.com>
2012-02-09 02:09:55 +0400

    disable cart check

--- htdocs/js/calculator/view.js
+++ htdocs/js/calculator/view.js
@@ -115,15 +115,15 @@ function CalculatorView() {
 		}, false);
 	}
   
-	function callBarmen (e)
-	{
-		if (Calculator.checkSum("call_barmen"))
-			return
-		
-		e.preventDefault()
-		alert("Минимальная сумма заказа составляет "+ Calculator.getMinSum("call_barmen") + ". Добавьте что-нибудь еще ;-)");
-	}
-	$('call_barmen').addEventListener('click', callBarmen, false);
+	// function callBarmen (e)
+	// {
+	// 	if (Calculator.checkSum("call_barmen"))
+	// 		return
+	// 	
+	// 	e.preventDefault()
+	// 	alert("Минимальная сумма заказа составляет "+ Calculator.getMinSum("call_barmen") + ". Добавьте что-нибудь еще ;-)");
+	// }
+	// $('call_barmen').addEventListener('click', callBarmen, false);
 	
 	/**
 	 * Событие, поступающее от модели в случае ее изменения
