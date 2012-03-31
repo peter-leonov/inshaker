c9c1e8b3f9dbb38022b3d0b04143bc5d9d0615d0
Peter Leonov <gojpeg@gmail.com>
2012-03-31 13:22:38 +0400

    popup code stub

--- htdocs/js/poll/poll.js
+++ htdocs/js/poll/poll.js
@@ -1,5 +1,32 @@
 ;(function(){
 
+var Me =
+{
+	bootstrap: function ()
+	{
+		var nodes =
+		{
+			root: document.body,
+			popupMain: $('poll-popup'),
+			popupParts:
+			{
+				window: $$('#poll-popup .popup-window')[0],
+				front: $$('#poll-popup .popup-front')[0]
+			}
+		}
+		
+		var popup = new Popup()
+		popup.bind(nodes)
+		
+		popup.show()
+	}
+}
 
+function onready ()
+{
+	Me.bootstrap()
+}
+
+document.addEventListener('ready', onready, false)
 
 })();
