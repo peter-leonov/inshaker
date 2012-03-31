0647681e31167fd5150a51a21098d166da8afa90
Peter Leonov <gojpeg@gmail.com>
2012-04-01 00:03:12 +0400

    finally, show this mfn' popup

--- htdocs/js/poll/poll.js
+++ htdocs/js/poll/poll.js
@@ -6,13 +6,9 @@ var Me =
 	{
 		var nodes =
 		{
-			root: document.body,
-			popupMain: $('poll-popup'),
-			popupParts:
-			{
-				window: $$('#poll-popup .popup-window')[0],
-				front: $$('#poll-popup .popup-front')[0]
-			}
+			root: $('poll-popup'),
+			window: $$('#poll-popup .popup-window')[0],
+			front: $$('#poll-popup .popup-front')[0]
 		}
 		
 		var popup = new Popup()
