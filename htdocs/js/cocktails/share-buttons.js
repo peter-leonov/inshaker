e3b295d5eab6aa0ec46f9e82fb67e66c608a46ed
Peter Leonov <gojpeg@gmail.com>
2012-04-23 22:16:54 +0400

    mark the link from share buttons

--- htdocs/js/cocktails/share-buttons.js
+++ htdocs/js/cocktails/share-buttons.js
@@ -8,8 +8,11 @@ var Me =
 		if (!frame || !frame[0]) // bad
 			return
 		
+		var l = window.location
+		var href = l.protocol + '//' + l.host + l.pathname + (l.search ? l.search + '&from=share-button' : '?from=share-button') + l.hash
+		
 		frame = frame[0]
-		frame.src = '/share-buttons.html?url=' + encodeURIComponent(location.href) + '&text=' + encodeURIComponent(params.text)
+		frame.src = '/share-buttons.html?url=' + encodeURIComponent(href) + '&text=' + encodeURIComponent(params.text)
 	}
 }
 
