b469266dcd7564497080d69c6528e2b004df542b
grabus <grabovalex@gmail.com>
2012-04-26 23:24:04 +0300

    backbone mvc blogpage

---
+++ htdocs/js/blog/blog-page/blog-page.js
@@ -0,0 +1,18 @@
+;(function(){
+
+<!--# include virtual="blog.js" -->
+
+function onready ()
+{
+	
+	var nodes =
+	{
+	}
+	
+	var widget = new BlogPage()
+	widget.bind(nodes)
+}
+
+$.onready(onready)
+
+})();
---
+++ htdocs/js/blog/blog-page/blog.js
@@ -0,0 +1,35 @@
+;(function(){
+
+var Papa
+
+;(function(){
+
+function Me ()
+{
+	var m = this.model = new Me.Model(),
+		v = this.view = new Me.View(),
+		c = this.controller = new Me.Controller()
+
+	m.view = v
+	v.controller = c
+	c.model = m
+
+	m.parent = v.parent = c.parent = this
+}
+
+Me.prototype =
+{
+}
+
+Me.className = 'BlogPage'
+self[Me.className] = Papa = Me
+
+})();
+
+
+<!--# include virtual="blog-model.js" -->
+<!--# include virtual="blog-view.js" -->
+<!--# include virtual="blog-controller.js" -->
+
+	
+})();
