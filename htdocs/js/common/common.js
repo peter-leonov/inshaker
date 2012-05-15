1a339dd38e165b6c9d743ad367e25126a753b100
Peter Leonov <gojpeg@gmail.com>
2012-05-15 21:48:13 +0400

    state machine stub

--- htdocs/js/common/common.js
+++ htdocs/js/common/common.js
@@ -18,6 +18,7 @@
 <!--# include virtual="/liby/modules/child-indexed-path.js"-->
 <!--# include virtual="/liby/modules/cloner.js"-->
 <!--# include virtual="/liby/modules/throttler.js" -->
+<!--# include virtual="/liby/modules/state-machine.js" -->
 
 if (!document.querySelectorAll)
 	$.load('/liby/core/fixes/dom-level2-selectors.js')
