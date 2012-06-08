08ac0d32c7a16c0326eed5f449d1f53e4ca3d516
grabus <grabovalex@gmail.com>
2012-06-08 05:14:25 +0300

    sort total

--- htdocs/js/rating/rating-page/rating-model.js
+++ htdocs/js/rating/rating-page/rating-model.js
@@ -4,6 +4,28 @@ function Me () {}
 
 Me.prototype =
 {
+	rating: <!--# include virtual="/db/ratings/rating.json" -->,
+	ingredients: <!--# include virtual="/db/ratings/ingredients.json" -->,
+	tags: <!--# include virtual="/db/ratings/tags.json" -->,
+	
+	sortByPos: function()
+	{
+		var cocktails = []
+		for (var k in this.rating)
+		{
+			var q = {}
+			q.name = k
+			q.days = this.rating[k]
+			cocktails.push(q)
+		}
+		
+		this.cocktails = cocktails.sort(this.sort)
+	},
+	
+	sort: function(a, b)
+	{
+		return a.days[0] - b.days[0]
+	}
 }
 
 Papa.Model = Me
