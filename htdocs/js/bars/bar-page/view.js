c9c2a91a19266b8ef876eb0421e006b91fdbdb34
Peter Leonov <gojpeg@gmail.com>
2012-06-08 13:32:55 +0400

    garbage collected

--- htdocs/js/bars/bar-page/view.js
+++ htdocs/js/bars/bar-page/view.js
@@ -17,8 +17,6 @@ Me.prototype =
 		share.render(window.location.href, 'Бар «' + nodes.barName.getAttribute('data-value') + '»')
 		
 		this.renderPhotos()
-		
-		nodes.barPrev.hide = nodes.barNext.hide = function () { this.addClassName('hidden') }
 	},
 	
 	renderPhotos: function ()
--- htdocs/js/event/event-page/event-page.js
+++ htdocs/js/event/event-page/event-page.js
@@ -20,56 +20,9 @@ EventPage =
 
 $.onready(function ()
 {
-	var de = document.documentElement
-	de.removeClassName('loading')
-	de.addClassName(guessBrowser(navigator.userAgent).join(' '))
-	de.addClassName(guessOS(navigator.userAgent).join(' '))
+	document.documentElement.removeClassName('loading')
+	UserAgent.setupDocumentElementClassNames()
 })
-
-function guessOS (ua)
-{
-	var m = /Windows/.exec(ua)
-	if (m)
-		return ['win']
-	
-	var m = /Macintosh|Mac OS/.exec(ua)
-	if (m)
-		return ['mac']
-}
-
-function guessBrowser (ua)
-{
-	function classes (n, a, b, c)
-	{
-		return [n, n + '-' + a, n + '-' + a + '-' + b, n + '-' + a + '-' + b + '-' + c]
-	}
-	
-	var m = /Firefox\/(\d+)\.(\d+)\.(\d+)/.exec(ua)
-	if (m)
-		return classes('firefox', m[1], m[2], m[3])
-	
-	var m = /Opera\/\d+.+Version\/(\d+)\.(\d)(\d)/.exec(ua)
-	if (m)
-		return classes('opera', m[1], m[2], m[3])
-	
-	var m = /Opera\/(\d+)\.(\d)(\d)/.exec(ua)
-	if (m)
-		return classes('opera', m[1], m[2], m[3])
-	
-	var m = /MSIE (\d+)\./.exec(ua)
-	if (m)
-		return ['msie', 'msie-' + m[1]]
-	
-	var m = /Version\/(\d+)\.(\d+)\.(\d+) Safari\/\d+/.exec(ua)
-	if (m)
-		return classes('safari', m[1], m[2], m[3])
-	
-	var m = /Chrome\/(\d+)\.(\d+)\.(\d+)/.exec(ua)
-	if (m)
-		return classes('chrome', m[1], m[2], m[3])
-	
-	return []
-}
 
 $.onready
 (
