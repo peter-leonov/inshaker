be4b681fe29f617c494962703225244993380cdf
grabus <grabovalex@gmail.com>
2012-06-08 05:15:53 +0300

    add total arrow

--- htdocs/js/rating/rating-page/rating-model.js
+++ htdocs/js/rating/rating-page/rating-model.js
@@ -25,6 +25,30 @@ Me.prototype =
 	sort: function(a, b)
 	{
 		return a.days[0] - b.days[0]
+	},
+	
+	addTotalArrow: function()
+	{
+		var cocktails = this.cocktails.slice(0, 10) // ten cocktails
+		
+		for (var i = 0, il = cocktails.length; i < il; i++)
+		{
+			var c = cocktails[i].days
+			
+			for (var j = 0, jl = c.length-1; j < jl; j++)
+			{
+				if (c[j] < c[j+1])
+				{
+					cocktails[i].totalArrow = 'up'
+					break
+				}
+				else if (c[j] > c[j+1])
+				{
+					cocktails[i].totalArrow = 'down'
+					break
+				}
+			}
+		}
 	}
 }
 
