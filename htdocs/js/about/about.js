6a87c82524406700b74ddb570010987d86baee1d
grabus <grabovalex@gmail.com>
2012-08-24 17:02:39 +0300

    remove all for charts

--- htdocs/js/about/about.js
+++ htdocs/js/about/about.js
@@ -39,59 +39,6 @@ var AboutPage = {
 		
 		locationHash.addEventListener('change', function () { window.scrollTo(0, 0); sw.select(hrefs.indexOf(this.get())) }, false)
 		
-		var statCities = $('#stat_cities'),
-			statVisits = $('#stat_visits')
-		
-		google.setOnLoadCallback(drawCharts)
-		
-		function drawCharts ()
-		{
-			var visual = google.visualization
-			
-			var dataPie = new visual.DataTable()
-			dataPie.addColumn('string', 'City')
-			dataPie.addColumn('number', 'Slices')
-			dataPie.addRows(cities)
-			
-			var optionsPie = {
-				width: 510,
-				height: 400,
-				chartArea:
-				{
-					left: 15,
-					width: 1000
-				}
-			}
-			
-			var chartPie = new visual.PieChart(statCities)
-			chartPie.draw(dataPie, optionsPie)
-			
-			
-			var dataArea = new visual.DataTable()
-			dataArea.addColumn('string', 'Дата')
-			dataArea.addColumn('number', 'Просмотры')
-			dataArea.addColumn('number', 'Визиты')
-			dataArea.addRows(visitors)
-			
-			var optionsArea = {
-				focusTarget: 'category',
-				width: 510,
-				height: 400,
-				legend:
-				{
-					position: 'bottom',
-					alignment: 'center'
-				},
-				chartArea:
-				{
-					left: 95,
-					width: 370
-				}
-			}
-			
-			var chartArea = new visual.AreaChart(statVisits)
-			chartArea.draw(dataArea, optionsArea)
-		}
 		
 		var form = $('#feedback_form')
 		function sendListener (e)
