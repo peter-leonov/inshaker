f450fe4d0e96639f9ca78a1db7ae1a569a65e1a3
Peter Leonov <gojpeg@gmail.com>
2012-08-26 00:03:55 +0400

    garbage collected

--- htdocs/js/combinator/combinator-page/model.js
+++ htdocs/js/combinator/combinator-page/model.js
@@ -34,7 +34,6 @@ Me.prototype =
 		'по группам',
 		'по методу приготовления',
 		'по алфавиту'
-		// 'по количеству ингредиента'
 	],
 	
 	sortTypeByNum:
@@ -161,10 +160,8 @@ Me.prototype =
 			break
 		}
 		
-		var stats = this.calculateStats(cocktails)
-		
 		// oowf, need to update the view
-		this.view.renderCocktails(sorted, cocktails.length, stats)
+		this.view.renderCocktails(sorted, cocktails.length)
 	},
 	
 	sortByIncreasingComplexity: function (cocktails)
@@ -261,42 +258,6 @@ Me.prototype =
 		}
 		
 		return groups
-	},
-	
-	calculateStats: function (cocktails)
-	{
-		var rating = {}
-		for (var i = 0, il = cocktails.length; i < il; i++)
-		{
-			var parts = cocktails[i].ingredients
-			for (var j = 0, jl = parts.length; j < jl; j++)
-			{
-				var name = parts[j][0]
-				var count = rating[name]
-				if (count)
-					rating[name]++
-				else
-					rating[name] = 1
-			}
-		}
-		
-		var all = Object.keys(rating)
-		all.sort(function (a, b) { return rating[b] - rating[a] })
-		all = all.slice(0, 7)
-		
-		var top = []
-		for (var i = 0, il = all.length; i < il; i++)
-		{
-			var name = all[i]
-			
-			top[i] =
-			{
-				ingredient: Ingredient.getByName(name),
-				rating: rating[name]
-			}
-		}
-		
-		return {top: top}
 	},
 	
 	combine: function (arr)
