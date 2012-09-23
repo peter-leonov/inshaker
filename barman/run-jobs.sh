b4efd7c99a7d68ccbd3394dae65e78c1b5b40e71
Peter Leonov <gojpeg@gmail.com>
2012-09-23 16:49:03 +0400

    garbage collected

--- barman/run-jobs.sh
+++ barman/run-jobs.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 
 curl -s --user "worker:$BARMAN_PASS" --data "job=analytics" http://m.barman.inshaker.ru/act/job.cgi
-curl -s --user "worker:$BARMAN_PASS" --data "job=stats"     http://m.barman.inshaker.ru/act/job.cgi
-
