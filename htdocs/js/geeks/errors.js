c324c031136868e5fd79ef02b750ea9d1f63e273
grabus <grabovalex@gmail.com>
2012-10-21 19:16:45 +0300

    add dummy errors.js

---
+++ htdocs/js/geeks/errors.js
@@ -0,0 +1,15 @@
+;(function(){
+
+eval(NodesShortcut.include())
+
+var Me =
+{
+
+}
+
+Me.className = 'TopErrors'
+self[Me.className] = Me
+
+Me.initialize(<!--# include virtual="/db/stats/errors.json" -->)
+
+})();
