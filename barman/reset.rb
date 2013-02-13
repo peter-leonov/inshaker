0745923d73071092962a0c11d50f7de8208f9383
Peter Leonov <gojpeg@gmail.com>
2013-02-13 19:39:52 +0200

    try to unlock

--- barman/reset.rb
+++ barman/reset.rb
@@ -1,9 +1,15 @@
 #!/usr/bin/env ruby1.9
 # encoding: utf-8
+$:.push('/www/inshaker/barman')
+
 require 'inshaker'
 require "lib/checker"
 
 class ResetState < Inshaker::Processor
+  module GlobalConfig
+    include Inshaker
+    include Inshaker::Launcher
+  end
   module Config
     ROOT_DIR = Inshaker::ROOT_DIR
   end
@@ -16,6 +22,12 @@ class ResetState < Inshaker::Processor
     
     Dir.chdir("#{Config::ROOT_DIR}")
     
+    if File.exists? GlobalConfig::LOCKPATH
+      pid = File.read(GlobalConfig::LOCKPATH_PID)
+      say "гашу предыдущий процесс (#{pid})"
+      
+      FileUtils.rmtree(GlobalConfig::LOCKPATH)
+    end
     say "сбрасываю все изменения…"
     system("git fetch")
     system("git reset --hard git/master")
