49ff3d96b0602475fb0bd67b295df762542c45b8
Peter Leonov <gojpeg@gmail.com>
2013-03-26 00:58:37 +0200

    grabbing bars page

---
+++ htdocs/js/shop/shop-page/shop-page.js
@@ -0,0 +1,44 @@
+<!--# include virtual="/liby/modules/google-api-loader.js" -->
+
+<!--# include virtual="/liby/widgets/map.js" -->
+<!--# include virtual="/liby/widgets/map-light-marker.js" -->
+
+<!--# include virtual="/js/common/google.js" -->
+
+
+
+$.onready(function ()
+{
+	UserAgent.setupDocumentElementClassNames()
+	
+	var nodes =
+	{
+		map: $('#map'),
+		mapSurface: $('#map .surface'),
+		positionControl: $('.position-control')
+	}
+	
+	RoundedCorners.round(nodes.map)
+	
+	var widget = new ShopPage(nodes)
+	widget.render()
+})
+
+;(function(){
+
+function ShopPage (nodes)
+{
+	this.nodes = nodes
+}
+
+ShopPage.prototype =
+{
+	render: function ()
+	{
+		
+	}
+}
+
+window.ShopPage = ShopPage
+
+})();
