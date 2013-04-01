bdc8a9ae5390ddfebc807df49dd6176225c882ac
Peter Leonov <gojpeg@gmail.com>
2013-04-01 22:38:11 +0300

    more accurate point and zoom

--- htdocs/js/shop/shop-page/shop-page.js
+++ htdocs/js/shop/shop-page/shop-page.js
@@ -68,7 +68,7 @@ ShopPage.prototype =
 		
 		var map = this.map = new Map()
 		map.bind({main: this.nodes.mapSurface, wrapper: this.nodes.map, control: this.nodes.positionControl})
-		map.setCenter({lat: 55.751755, lng: 37.624657}, 9)
+		map.setCenter({lat: 55.678951, lng: 37.641293}, 14)
 		
 		var shop =
 		{
@@ -78,7 +78,7 @@ ShopPage.prototype =
 				address: this.nodes.address.firstChild.nodeValue,
 				tel: this.nodes.phone.firstChild.nodeValue
 			},
-			point: [55.766212, 37.640148]
+			point: [55.678951, 37.641293]
 		}
 		map.setPoints([new ShopPoint(shop)])
 	}
