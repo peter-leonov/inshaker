24ea96036a8e1e48300b9e08706e59b75f60f575
Peter Leonov <gojpeg@gmail.com>
2013-04-21 23:38:21 +0300

    wrap in a class

--- storage/import.rb
+++ storage/import.rb
@@ -13,63 +13,70 @@ module Oj
   end
 end
 
-$db = SQLite3::Database.new 'storage.sqlite3'
-
-$db.execute <<-SQL
-BEGIN;
-
-CREATE TABLE IF NOT EXISTS Nodes
-(
-  id       INTEGER PRIMARY KEY,
-  created  INTEGER,
-  modified INTEGER,
-  node     TEXT,
-  key      TEXT,
-  json     TEXT
-);
-
-CREATE INDEX IF NOT EXISTS node_with_key ON Nodes(node, key);
-
-COMMIT;
-SQL
-
-db_insert_stmt = $db.prepare( "INSERT INTO Nodes(node, key, json) VALUES(:node, :key, :json)" )
-
-def traverse
-  Find.find("/Users/peter/Desktop/db/").each do |path|
-    m = %r{/(\w+)/bar\.json$}.match path
-    next unless m
-    node = m[1]
-    key = 'bar'
+class Import
+  module Config
+    REPO_PATH = "/Users/peter/Desktop/db/"
+  end
+  
+  def initialize
+    @db = SQLite3::Database.new 'storage.sqlite3'
     
-    json = File.read(path)
-    begin
-      Oj.parse(json)
-    rescue => e
-      if json == ""
-        {}
-      else
-        begin
-          json.gsub! 'undefined', 'null'
-          json.gsub! %r[}GET.*$]m, '}'
-        rescue => e
-          puts "broken JSON in #{node}"
-          next
-        end
+    @db.execute <<-SQL
+      BEGIN;
+
+      CREATE TABLE IF NOT EXISTS Nodes
+      (
+        id       INTEGER PRIMARY KEY,
+        created  INTEGER,
+        modified INTEGER,
+        node     TEXT,
+        key      TEXT,
+        json     TEXT
+      );
+
+      CREATE INDEX IF NOT EXISTS node_with_key ON Nodes(node, key);
+
+      COMMIT;
+    SQL
+    
+    @insert_stmt = @db.prepare( "INSERT INTO Nodes(node, key, json) VALUES(:node, :key, :json)" )
+  end
+
+  def traverse
+    Find.find(Config::REPO_PATH).each do |path|
+      m = %r{/(\w+)/bar\.json$}.match path
+      next unless m
+      node = m[1]
+      key = 'bar'
+    
+      json = File.read(path)
+      begin
+        Oj.parse(json)
+      rescue => e
+        if json == ""
+          {}
+        else
+          begin
+            json.gsub! 'undefined', 'null'
+            json.gsub! %r[}GET.*$]m, '}'
+          rescue => e
+            puts "broken JSON in #{node}"
+            next
+          end
         
-        begin
-          Oj.parse(json)
-        rescue => e
-          puts "broken JSON in #{node}"
-          puts json
-          next
+          begin
+            Oj.parse(json)
+          rescue => e
+            puts "broken JSON in #{node}"
+            puts json
+            next
+          end
         end
       end
-    end
     
-    db_insert_stmt.execute node: node, key: key, json: json
+      @insert_stmt.execute node: node, key: key, json: json
+    end
   end
 end
 
-
-traverse
+Import.new.traverse
