b571997a666f24d6f6be61c369791d0f8ef2b8d8
Peter Leonov <gojpeg@gmail.com>
2013-10-28 02:56:23 +0400

    eliminate this.nodes

--- htdocs/js/bars/point.js
+++ htdocs/js/bars/point.js
@@ -6,7 +6,6 @@ function Me (bar)
 {
 	this.bar = bar
 	this.ll = {lat: bar.point[0], lng: bar.point[1]}
-	this.nodes = {}
 }
 
 Me.prototype = new MapLightMarker()
@@ -24,25 +23,24 @@ var myProto =
 	
 	getNode: function ()
 	{
-		var nodes = this.nodes,
-			bar = this.bar
+		var bar = this.bar
 		
-		var main = nodes.main = Nc('div', 'point')
+		var main = Nc('div', 'point')
 		main.addEventListener('mousedown', stopPropagation, false)
 		
-		var icon = nodes.icon = main.appendChild(Nc('a', 'icon'))
+		var icon = main.appendChild(Nc('a', 'icon'))
 		icon.href = this.bar.pageHref()
 		
-		var wider = nodes.title = main.appendChild(Nc('dl', 'wider'))
-		var title = nodes.title = wider.appendChild(Nc('dl', 'title'))
+		var wider = main.appendChild(Nc('div', 'wider'))
+		var title = wider.appendChild(Nc('dl', 'title'))
 		
-		var name = nodes.name = title.appendChild(Nct('dt', 'point-name', bar.name))
+		var name = title.appendChild(Nct('dt', 'point-name', bar.name))
 		
 		var contacts = bar.contacts
 		if (contacts)
 		{
-			nodes.address = title.appendChild(Nct('dd', 'address', contacts.address))
-			nodes.tel = title.appendChild(Nct('dd', 'tel', contacts.tel))
+			title.appendChild(Nct('dd', 'address', contacts.address))
+			title.appendChild(Nct('dd', 'tel', contacts.tel))
 		}
 		
 		return main
