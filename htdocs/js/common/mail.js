9ac140554a3d47cdbd78877fe17edbe6c4b44806
Peter Leonov <gojpeg@gmail.com>
2013-12-10 07:03:54 +0400

    add log

--- htdocs/js/common/mail.js
+++ htdocs/js/common/mail.js
@@ -4,6 +4,7 @@ window.Mail =
 {
   send: function (message, sent)
   {
+    // console.log(message)
     // window.setTimeout(function () { sent({statusType: 'success'}) }, 250)
     return Request.post('/act/message', message, sent)
   }
