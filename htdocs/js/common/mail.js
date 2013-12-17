7c465ddc322742353c8087cd5a5abd3db9a863cc
Peter Leonov <gojpeg@gmail.com>
2013-12-18 02:08:30 +0400

    attach the debug information

--- htdocs/js/common/mail.js
+++ htdocs/js/common/mail.js
@@ -6,6 +6,8 @@ window.Mail =
   {
     // console.log(message)
     // window.setTimeout(function () { sent({statusType: 'success'}) }, 250)
+    message['h:X-User-Agent'] = window.navigator.userAgent
+    message['h:X-Client-Address'] = window.navigator.publicIP
     return Request.post('/act/message', message, sent)
   }
 }
