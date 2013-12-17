e05542a8d8b59f92d5d14d4891160a4aee02e645
Peter Leonov <gojpeg@gmail.com>
2013-12-18 01:42:59 +0400

    all in one commit
    hey Dr.Labs!

--- htdocs/w/questionnaire/questionnaire.js
+++ htdocs/w/questionnaire/questionnaire.js
@@ -3,47 +3,57 @@
 
 ;(function(){
 
-function QuestionnairePopup () {}
+function QuestionnairePopup (nodes)
+{
+  this.nodes = nodes
+  this.name = nodes.name.getAttribute('data-form-name')
+  this.title = nodes.name.firstChild.nodeValue
+}
 
 QuestionnairePopup.prototype =
 {
-  bind: function (nodes)
-  {
-    this.nodes = nodes
-    
-    this.name = nodes.name.getAttribute('data-poll-name')
-  },
-  
   show: function ()
   {
     var me = this
     
     this.nodes.form.addEventListener('submit', function (e) { me.onsubmit(e) }, false)
-    this.nodes.form.addEventListener('change', function (e) { me.onchange(e) }, false)
-    this.nodes.form.addEventListener('click', function (e) { me.onchange(e) }, false)
+    
+    // use label text as input names
+    var elements = this.nodes.form.elements
+    for (var i = 0, il = elements.length; i < il; i++)
+      elements[i].name = elements[i].parentNode.firstChild.nodeValue
     
     var popup = this.popup = new Popup()
     popup.bind(this.nodes.popup)
     popup.addEventListener('hide', function (e) { me.hide() }, false)
     popup.addEventListener('ui-hide', function (e) { me.hideFromUI() }, false)
     this.popup.show()
+    
+    // catch the adjusting scroll after load
+    function onscroll ()
+    {
+      popup.adjustPostion()
+      window.removeEventListener('scroll', onscroll, false)
+    }
+    window.addEventListener('scroll', onscroll, false)
+    window.setTimeout(function () { window.removeEventListener('scroll', onscroll, false) }, 1000)
   },
   
   hide: function ()
   {
-    Cookie.set(this.name + '-hidden', Date.now(), Date.add('28d'), '/')
+    window.localStorage.setItem('questionnaire.' + this.name + '.expires', +Date.add('10s')) // 28d
   },
   
   hideFromUI: function ()
   {
-    this.poll('hide')
+    Statistics.questionnaire(this.name, 'cancel')
     this.hide()
   },
   
   maybeShow: function ()
   {
-    var hidden = Cookie.get(this.name + '-hidden')
-    if (hidden)
+    var expires = window.localStorage.getItem('questionnaire.' + this.name + '.expires')
+    if (expires && new Date() < new Date(+expires))
       return
     
     if (Math.floor(Math.random() * 3) != 0)
@@ -55,27 +65,28 @@ QuestionnairePopup.prototype =
     this.show()
   },
   
-  onchange: function ()
-  {
-    this.nodes.button.disabled = false
-  },
-  
   onsubmit: function (e)
   {
     e.preventDefault()
     
-    var answer = FormHelper.toHash(e.target).answer
-    this.poll(answer)
+    var form = FormHelper.toHash(e.target)
+    
+    var messageBody = '<table cellspacing="5">'
+    for (var k in form)
+      messageBody += '<tr><td>' + k + '</td><td>' + form[k] + '</td></tr>'
+    messageBody += '</table>'
+    
+    Mail.send({
+      subject: 'Форма: ' + this.title,
+      to: 'pl@inshaker.ru',
+      from: 'Коктейльный сайт <mail@inshaker.ru>',
+      html: messageBody
+    }, function noop () {})
     
     this.nodes.root.classList.add('done')
     
     var popup = this.popup
     window.setTimeout(function () { popup.hide() }, 1000)
-  },
-  
-  poll: function (value)
-  {
-    Statistics.poll(this.name, value)
   }
 }
 
@@ -102,9 +113,7 @@ function onready ()
     }
   }
   
-  var widget = new QuestionnairePopup()
-  widget.bind(nodes)
-  
+  var widget = new QuestionnairePopup(nodes)
   widget.maybeShow()
 }
 
