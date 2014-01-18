ac8f7378c1c2cb6a2319f41a3189c3edcf634e0e
Peter Leonov <gojpeg@gmail.com>
2014-01-19 01:39:09 +0400

    use the same new widget

--- htdocs/js/shop/shop-page/good-page.js
+++ htdocs/js/shop/shop-page/good-page.js
@@ -1,12 +1,3 @@
-<!--# include virtual="/liby/modules/google-api-loader.js" -->
-
-<!--# include virtual="/liby/widgets/map.js" -->
-<!--# include virtual="/liby/widgets/map-light-marker.js" -->
-
-<!--# include virtual="/js/common/google.js" -->
-
-<!--# include virtual="shop-point.js" -->
-
 <!--# include virtual="/liby/modules/motion.js" -->
 <!--# include virtual="/liby/modules/motion-types.js" -->
 <!--# include virtual="/liby/modules/animation.js" -->
@@ -14,22 +5,12 @@
 
 <!--# include virtual="/w/delivery/delivery.js" -->
 
-<!--# include virtual="/w/plain-popup/plain-popup.js" -->
-
 $.onready(function ()
 {
 	UserAgent.setupDocumentElementClassNames()
 	
 	var nodes =
 	{
-		promo: $('#promo'),
-		
-		address: $('#promo .info .location p'),
-		phone: $('#promo .info .phone p'),
-		
-		map: $('#map .map'),
-		positionControl: $('.position-control'),
-		
 		cocktails: $('#cocktails .list'),
 		
 		photos: $('.photos')
@@ -52,31 +33,8 @@ GoodPage.prototype =
 {
 	render: function ()
 	{
-		this.initMap()
 		this.initCocktails()
 		this.bindScroller()
-	},
-	
-	initMap: function ()
-	{
-		if (this.map)
-			return
-		
-		var map = this.map = new Map()
-		map.bind({main: this.nodes.map, control: this.nodes.positionControl})
-		map.setCenter({lat: 55.783016, lng: 37.599892}, 14)
-		
-		var shop =
-		{
-			name: 'Коктейльный магазин',
-			contacts:
-			{
-				address: this.nodes.address.firstChild.nodeValue,
-				tel: this.nodes.phone.firstChild.nodeValue
-			},
-			point: [55.783016, 37.599892]
-		}
-		map.setPoints([new ShopPoint(shop)])
 	},
 	
 	initCocktails: function ()
