bce61f131a35951dd775d4dfb9b0d398f96de3b8
Peter Leonov <gojpeg@gmail.com>
2014-05-10 02:20:03 +0400

    make recipe the def state

--- htdocs/js/cocktails/cocktail-page/controller.js
+++ htdocs/js/cocktails/cocktail-page/controller.js
@@ -12,8 +12,8 @@ var Controller = {
 	
 	name : "",
 	relatedCount: 10,
-	lastFrame: 'state-initial',
-	defaultFrame: 'state-initial',
+	lastFrame: 'state-recipe',
+	defaultFrame: 'state-recipe',
 	
 	nodes:
 	{
