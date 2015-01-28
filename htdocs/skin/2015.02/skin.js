9068be1f96bad50446cc5bfcf80d50524f6e50e9
Peter Leonov <gojpeg@gmail.com>
2015-01-28 14:19:56 +0300

    stub

---
+++ htdocs/skin/2015.02/skin.js
@@ -0,0 +1,39 @@
+;(function(){
+
+var LINK = 'http://shop.inshaker.ru/'
+
+var Me =
+{
+  initialize: function (db)
+  {
+    this.db = db
+  },
+  
+  bind: function ()
+  {
+    var db = this.db
+    
+    for (var k in db)
+    {
+      var item = db[k]
+      if (!item.href)
+        continue
+      
+      var node = $(k)
+      if (node)
+      {
+        node.href = item.href
+      }
+    }
+  }
+}
+
+Me.initialize
+({
+  '#spotlighted': { 'href': LINK },
+  '#branded-image': { 'href': LINK }
+})
+
+Me.bind()
+
+})();
