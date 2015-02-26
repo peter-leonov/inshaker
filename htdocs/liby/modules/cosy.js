c3748af5c49280aad528570255eccdcbd23bc233
Peter Leonov <gojpeg@gmail.com>
2015-02-26 17:51:29 +0300

    relax

--- htdocs/liby/modules/cosy.js
+++ htdocs/liby/modules/cosy.js
@@ -16,16 +16,10 @@ $.onload = function (f) { return window.addEventListener('load', f, false) }
 $.onready = function (f) { document.addEventListener('DOMContentLoaded', f, false) }
 $.load = function (src)
 {
-	var me = arguments.callee
-	var cache = me.cache || (me.cache = {})
-	if (me.cache[src])
-		return me.cache[src]
-	var node = document.createElement('script')
-	node.type = 'text/javascript'
-	node.src = src
-	document.getElementsByTagName('head')[0].appendChild(node)
-	cache[src] = node
-	return node
+  var script = document.createElement('script')
+  script.src = src
+  document.getElementsByTagName('head')[0].appendChild(script)
+  return script
 }
 
 $.require = function (src)
