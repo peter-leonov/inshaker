ae7e475bb3a963afb636b384273ed176fa17d32c
Peter Leonov <gojpeg@gmail.com>
2015-10-31 20:23:00 +0100

    refactor to introduce more bugs

--- htdocs/skin/skin.js
+++ htdocs/skin/skin.js
@@ -1,22 +1,31 @@
 <!--# config timefmt="%Y.%m" -->
 !function(){
 
-var LINK = '<!--# include virtual="/skin/$date_local/link.txt" -->'
-var SELECTOR = '#spotlighted, #branded-image'
-
-var nodes = $$(SELECTOR)
-for (var i = 0, il = nodes.length; i < il; i++)
-  nodes[i].href = LINK
-
-var PINK_LINK = '<!--# include virtual="/skin/$date_local/pink-link.txt" -->'
-var PINK_TEXT = 'Только в ноябре: <u>набор супер бармена</u> всего за 1 999.-'
-var PINK_SLCR = '#pink-block'
-
-var nodes = $$(PINK_SLCR)
-for (var i = 0, il = nodes.length; i < il; i++)
+function branding ()
 {
-  nodes[i].innerHTML = PINK_TEXT
-  nodes[i].href      = PINK_LINK
+  var SELECTOR = '#spotlighted, #branded-image'
+  var LINK = '<!--# include virtual="/skin/$date_local/link.txt" -->'
+
+  var nodes = $$(SELECTOR)
+  for (var i = 0, il = nodes.length; i < il; i++)
+    nodes[i].href = LINK
 }
+branding()
+
+
+function pink ()
+{
+  var SELECTOR = '#pink-block'
+  var PINK = JSON.parse('<!--# include virtual="/skin/$date_local/pink.json" -->')
+  var random = PINK[Math.ceil(Math.random() * PINK.length)]
+
+  var nodes = $$(SELECTOR)
+  for (var i = 0, il = nodes.length; i < il; i++)
+  {
+    nodes[i].innerHTML = random.text
+    nodes[i].href      = random.link
+  }
+}
+try { pink() } catch (e) { /* I don't care */ }
 
 }();
