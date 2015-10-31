f3c4ab90a6f3ff7e3d84a321e6855cfdafe9a7cf
Peter Leonov <gojpeg@gmail.com>
2015-10-31 19:54:04 +0100

    copy branding logic

--- htdocs/skin/skin.js
+++ htdocs/skin/skin.js
@@ -8,4 +8,15 @@ var nodes = $$(SELECTOR)
 for (var i = 0, il = nodes.length; i < il; i++)
   nodes[i].href = LINK
 
+var PINK_LINK = '<!--# include virtual="/skin/$date_local/pink-link.txt" -->'
+var PINK_TEXT = 'Только в ноябре: <u>набор супер бармена</u> всего за 1 999.-'
+var PINK_SLCR = '#pink-block'
+
+var nodes = $$(PINK_SLCR)
+for (var i = 0, il = nodes.length; i < il; i++)
+{
+  nodes[i].innerHTML = PINK_TEXT
+  nodes[i].href      = PINK_LINK
+}
+
 }();
